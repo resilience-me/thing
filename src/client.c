035a6b5a6a6b6f61abb043a22d0b231a89873cf6
resilience-me <emailsareobsolete@gmail.com>
2024-05-09 12:59:15 +0200

    Update client.c

--- src/client.c
+++ src/client.c
@@ -1,74 +1,52 @@
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
+#include <unistd.h>
 #include <arpa/inet.h>
 #include <sys/socket.h>
-
-#define SERVER "172.234.123.103"
-#define BUFLEN 512  // Max length of buffer
-#define PORT 8888   // The port on which to send data
-
-#define OPCODE_MESSAGE 1
-#define OPCODE_GET_LAST_CMD 2
-#define OPCODE_SET_TRUSTLINE 3
-#define OPCODE_PAY 4
 
 void die(char *s) {
     perror(s);
     exit(1);
 }
 
-typedef struct {
-    int opcode;
-    char data[BUFLEN];
-} Command;
-
 int main(void) {
     struct sockaddr_in si_other;
-    int s, slen = sizeof(si_other);
-    Command cmd;
+    int s, slen=sizeof(si_other);
+    char buf[512];
+    char message[512];
 
-    if ((s = socket(AF_INET, SOCK_DGRAM, IPPROTO_UDP)) == -1) {
+    if ((s=socket(AF_INET, SOCK_DGRAM, IPPROTO_UDP)) == -1) {
         die("socket");
     }
 
     memset((char *) &si_other, 0, sizeof(si_other));
     si_other.sin_family = AF_INET;
-    si_other.sin_port = htons(PORT);
+    si_other.sin_port = htons(8888);
 
-    if (inet_aton(SERVER, &si_other.sin_addr) == 0) {
+    if (inet_aton("172.234.123.103", &si_other.sin_addr) == 0) {
         fprintf(stderr, "inet_aton() failed\n");
         exit(1);
     }
 
-    while (1) {
-        printf("Enter opcode (1-Message, 2-Get Last Cmd, 3-Set Trustline, 4-Pay): ");
-        scanf("%d", &cmd.opcode);
-        getchar(); // clear the newline character after the number
+    while(1) {
+        printf("Enter message: ");
+        if (fgets(message, sizeof(message), stdin) == NULL) break;  // Properly use fgets to avoid buffer overflow
+        message[strcspn(message, "\n")] = 0;  // Remove newline character
 
-        if (cmd.opcode != OPCODE_GET_LAST_CMD) {
-            printf("Enter message: ");
-            fgets(cmd.data, BUFLEN, stdin);
-            // Remove newline at the end of input if present
-            size_t len = strlen(cmd.data);
-            if (len > 0 && cmd.data[len-1] == '\n') {
-                cmd.data[len-1] = '\0';
-            }
-        } else {
-            strcpy(cmd.data, ""); // Send empty data for GET_LAST_CMD
-        }
-
-        // Send the command
-        if (sendto(s, &cmd, sizeof(cmd), 0, (struct sockaddr *) &si_other, slen) == -1) {
+        // Send the message
+        if (sendto(s, message, strlen(message), 0 , (struct sockaddr *) &si_other, slen) == -1) {
             die("sendto()");
         }
 
-        // Receive the response
-        if (recvfrom(s, cmd.data, BUFLEN, 0, (struct sockaddr *) &si_other, &slen) == -1) {
+        // Clear the buffer by filling null, it might have previously received data
+        memset(buf, '\0', 512);
+        // Try to receive some data, this is a blocking call
+        if (recvfrom(s, buf, 512, 0, (struct sockaddr *) &si_other, &slen) == -1) {
             die("recvfrom()");
         }
 
-        printf("Server reply: %s\n", cmd.data);
+        puts(buf);
     }
 
     close(s);
