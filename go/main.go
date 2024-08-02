46fe8b2c405ac1e82434c4a73493c3064b381a84
resilience-me <emailsareobsolete@gmail.com>
2024-08-03 00:34:41 +0200

    Update main.go

--- go/main.go
+++ go/main.go
@@ -1,11 +1,12 @@
 package main
 
 import (
-    "encoding/binary"
     "fmt"
     "net"
+    "os"
 )
 
+// Datagram structure to define the packet structure
 type Datagram struct {
     Command        byte
     XUsername      [32]byte
@@ -16,51 +17,51 @@ type Datagram struct {
     Signature      [32]byte
 }
 
+// Define the type for command handlers
 type CommandHandler func(Datagram, *net.UDPAddr)
 
+// Array of command handlers, assuming commands 0 and 1 are implemented
 var commandHandlers = []CommandHandler{
     handleSetTrustline, // Command 0
     handleGetTrustline, // Command 1
-}
-
-func handleSetTrustline(dg Datagram, addr *net.UDPAddr) {
-    trustlineAmount := binary.LittleEndian.Uint32(dg.Arguments[:4])
-    fmt.Printf("Set trustline to %d for %s\n", trustlineAmount, dg.YUsername)
-}
-
-func handleGetTrustline(dg Datagram, addr *net.UDPAddr) {
-    fmt.Printf("Get trustline for %s\n", dg.YUsername)
+    // More handlers can be added here sequentially
 }
 
 func main() {
+    // Listening on both IPv4 and IPv6 addresses
     addr := net.UDPAddr{
         Port: 2012,
-        IP:   net.ParseIP("::"),  // Listen on all IPv6 addresses and dual-stack for IPv4
+        IP:   net.ParseIP("::"), // This handles both IPv6 and IPv4
     }
+
+    // Setup the UDP server
     conn, err := net.ListenUDP("udp", &addr)
     if err != nil {
-        fmt.Println(err)
+        fmt.Fprintf(os.Stderr, "Error listening on UDP port %d: %v\n", addr.Port, err)
         return
     }
     defer conn.Close()
 
+    fmt.Printf("Server is listening on all network interfaces for both IPv4 and IPv6 at port %d\n", addr.Port)
+
+    // Infinite loop to handle incoming datagrams
     for {
         var dg Datagram
-        _, remoteAddr, err := conn.ReadFromUDP(dg[:])
+        n, remoteAddr, err := conn.ReadFromUDP(dg[:])
         if err != nil {
-            fmt.Println(err)
+            fmt.Fprintf(os.Stderr, "Error reading from UDP: %v\n", err)
+            continue
+        }
+        if n < len(dg) {
+            fmt.Println("Received incomplete datagram")
             continue
         }
 
-        fmt.Printf("Received datagram from %s\n", remoteAddr.String())
-        dispatchCommand(dg, remoteAddr)
-    }
-}
-
-func dispatchCommand(dg Datagram, addr *net.UDPAddr) {
-    if int(dg.Command) < len(commandHandlers) {
-        commandHandlers[dg.Command](dg, addr)
-    } else {
-        fmt.Printf("Unknown command: %d\n", dg.Command)
+        // Dispatch the command using an array index
+        if int(dg.Command) < len(commandHandlers) {
+            commandHandlers[dg.Command](dg, remoteAddr)
+        } else {
+            fmt.Printf("Unknown command: %d\n", dg.Command)
+        }
     }
 }
