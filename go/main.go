bf6157cf4be8d43304feeab00543d63ff5e9e209
resilience-me <emailsareobsolete@gmail.com>
2024-08-03 02:44:41 +0200

    Update main.go

--- go/main.go
+++ go/main.go
@@ -5,25 +5,6 @@ import (
     "net"
 )
 
-type Datagram struct {
-    Command        byte
-    XUsername      [32]byte
-    YUsername      [32]byte
-    YServerAddress [32]byte
-    Arguments      [256]byte
-    Counter        [4]byte
-    Signature      [32]byte
-}
-
-type CommandHandler func(Datagram, *net.UDPAddr)
-
-// Direct initialization of commandHandlers
-var commandHandlers = [256]CommandHandler{
-    handleSetTrustline, // Command 0
-    handleGetTrustline, // Command 1
-    // All other handlers are implicitly set to nil
-}
-
 func main() {
     addr := net.UDPAddr{
         Port: 2012,
