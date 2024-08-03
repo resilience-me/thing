25aae81689c84f3fd19003b0a9659f3526da095b
resilience-me <emailsareobsolete@gmail.com>
2024-08-03 11:03:25 +0200

    Update setTrustline.go

--- go/handlers/server/setTrustline.go
+++ go/handlers/server/setTrustline.go
@@ -31,10 +31,14 @@ func SetTrustline(dg main.Datagram, addr *net.UDPAddr) {
         return
     }
 
+    // Get the trustline directory
     trustlineDir := filepath.Join(peerDir, "trustline")
+
+    // Construct the trustline and counter file paths
     counterOutPath := filepath.Join(trustlineDir, "counter_out.txt")
     trustlineOutPath := filepath.Join(trustlineDir, "trustline_out.txt")
 
+    // Load the previous counter value
     prevCounterStr, err := os.ReadFile(counterOutPath)
     if err != nil && !os.IsNotExist(err) {
         fmt.Printf("Error reading counter file: %v\n", err)
@@ -45,17 +49,20 @@ func SetTrustline(dg main.Datagram, addr *net.UDPAddr) {
         prevCounter = int(binary.BigEndian.Uint32(prevCounterStr))
     }
 
+    // Check the counter
     counter := binary.BigEndian.Uint32(dg.Counter[:])
     if int(counter) <= prevCounter {
         fmt.Println("Received counter is not greater than previous counter. Potential replay attack.")
         return
     }
 
+    // Write the new trustline amount to the file
     if err := os.WriteFile(trustlineOutPath, []byte(fmt.Sprintf("%d", trustlineAmount)), 0644); err != nil {
         fmt.Printf("Error writing trustline to file: %v\n", err)
         return
     }
 
+    // Write the new counter value as a string
     counterStr := fmt.Sprintf("%d", counter)
     if err := os.WriteFile(counterOutPath, []byte(counterStr), 0644); err != nil {
         fmt.Printf("Error writing counter to file: %v\n", err)
