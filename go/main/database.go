4502e0c0bccd2f2e9c8615e8f694413ade53a1b8
resilience-me <emailsareobsolete@gmail.com>
2024-08-05 06:26:47 +0200

    Update database.go

--- go/main/database.go
+++ go/main/database.go
@@ -9,29 +9,37 @@ import (
 var datadir = filepath.Join(os.Getenv("HOME"), "resilience")
 
 // GetAccountDir constructs the account directory path from the datagram
-func GetAccountDir(dg *Datagram) (string, error) { // Change to pointer
+func GetAccountDir(dg *Datagram) string { // Change to pointer
     username := string(dg.XUsername[:])
     accountDir := filepath.Join(datadir, "accounts", username)
 
-    // Ensure the account directory exists
-    if _, err := os.Stat(accountDir); err != nil {
-        return "", err
-    }
-
-    return accountDir, nil
+    return accountDir
 }
 
-// GetPeerDir constructs the peer directory path from the datagram and checks if it exists.
-func GetPeerDir(dg *Datagram, accountDir string) (string, error) { // Change to pointer
+// GetPeerDir constructs the peer directory path from the datagram and returns it
+func GetPeerDir(dg *Datagram, accountDir string) string { // Change to pointer
     peerUsername := string(dg.YUsername[:])
     peerAddress := string(dg.YServerAddress[:])
 
     peerDir := filepath.Join(accountDir, "peers", peerAddress, peerUsername)
 
-    // Ensure the peer directory exists
-    if _, err := os.Stat(peerDir); err != nil {
-        return "", err
-    }
+    return peerDir
+}
 
-    return peerDir, nil
+// CheckAccountExists checks if the account directory exists
+func CheckAccountExists(accountDir string) error {
+    // Ensure the account directory exists
+    if err := os.Stat(accountDir); err != nil {
+        return err
+    }
+    return nil
+}
+
+// CheckPeerExists checks if the peer directory exists
+func CheckPeerExists(peerDir string) error {
+    // Ensure the peer directory exists
+    if err := os.Stat(peerDir); err != nil {
+        return err
+    }
+    return nil
 }
