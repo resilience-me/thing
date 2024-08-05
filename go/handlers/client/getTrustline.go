1ce3be9eaf3cc9ca9a3000aa10b5cebb0184a700
resilience-me <emailsareobsolete@gmail.com>
2024-08-05 05:42:02 +0200

    Update getTrustline.go

--- go/handlers/client/getTrustline.go
+++ go/handlers/client/getTrustline.go
@@ -1,46 +1,46 @@
-// package handlers
+package client
 
-// import (
-//     "net"
-//     "os"
-//     "path/filepath"
-//     "resilience/main"
-// )
+import (
+    "net"
+    "os"
+    "path/filepath"
+    "resilience/main"
+)
 
-// // Handles fetching the outbound trustline information
-// func GetTrustlineOut(dg main.Datagram, conn *net.UDPConn, addr *net.UDPAddr) {
-//     peerDir, err := main.GetPeerDir(dg)
-//     if err != nil {
-//         return // Optionally handle or log the error
-//     }
+// Handles fetching the outbound trustline information
+func GetTrustlineOut(dg main.Datagram, conn *net.UDPConn, addr *net.UDPAddr) {
+    peerDir, err := main.GetPeerDir(dg)
+    if err != nil {
+        return // Optionally handle or log the error
+    }
 
-//     trustlineOutPath := filepath.Join(peerDir, "trustline", "trustline_out.txt")
-//     trustlineAmount, err := os.ReadFile(trustlineOutPath)
-//     if err != nil {
-//         return // Optionally handle or log the error
-//     }
+    trustlineOutPath := filepath.Join(peerDir, "trustline", "trustline_out.txt")
+    trustlineAmount, err := os.ReadFile(trustlineOutPath)
+    if err != nil {
+        return // Optionally handle or log the error
+    }
 
-//     _, err = conn.WriteToUDP(trustlineAmount, addr)
-//     if err != nil {
-//         return // Optionally handle or log the error
-//     }
-// }
+    _, err = conn.WriteToUDP(trustlineAmount, addr)
+    if err != nil {
+        return // Optionally handle or log the error
+    }
+}
 
-// // Handles fetching the inbound trustline information
-// func GetTrustlineIn(dg main.Datagram, conn *net.UDPConn, addr *net.UDPAddr) {
-//     peerDir, err := main.GetPeerDir(dg)
-//     if err != nil {
-//         return // Optionally handle or log the error
-//     }
+// Handles fetching the inbound trustline information
+func GetTrustlineIn(dg main.Datagram, conn *net.UDPConn, addr *net.UDPAddr) {
+    peerDir, err := main.GetPeerDir(dg)
+    if err != nil {
+        return // Optionally handle or log the error
+    }
 
-//     trustlineInPath := filepath.Join(peerDir, "trustline", "trustline_in.txt")
-//     trustlineAmount, err := os.ReadFile(trustlineInPath)
-//     if err != nil {
-//         return // Optionally handle or log the error
-//     }
+    trustlineInPath := filepath.Join(peerDir, "trustline", "trustline_in.txt")
+    trustlineAmount, err := os.ReadFile(trustlineInPath)
+    if err != nil {
+        return // Optionally handle or log the error
+    }
 
-//     _, err = conn.WriteToUDP(trustlineAmount, addr)
-//     if err != nil {
-//         return // Optionally handle or log the error
-//     }
-// }
+    _, err = conn.WriteToUDP(trustlineAmount, addr)
+    if err != nil {
+        return // Optionally handle or log the error
+    }
+}
