682c3ef7996d3e74b9fb2ae7ffeb43f920cb8e7b
resilience-me <emailsareobsolete@gmail.com>
2024-08-06 00:00:16 +0200

    Update server_send.go

--- go/handlers/server_send.go
+++ go/handlers/server_send.go
@@ -1,15 +1,12 @@
-// SignAndSendDatagram signs and sends a Datagram over UDP.
 func SignAndSendDatagram(ctx main.HandlerContext, dg *main.Datagram) error {
     // Sign the datagram
     if err := main.SignDatagram(dg); err != nil {
-        return fmt.Errorf("Failed to sign datagram: %v\n", err)
+        return fmt.Errorf("failed to sign datagram: %v", err)
     }
 
     // Send the datagram back to the peer
-    _, err := ctx.Conn.WriteToUDP(dg[:], ctx.Addr)
-    if err != nil {
-        fmt.Printf("Error sending datagram: %v\n", err)
-        return err
+    if _, err := ctx.Conn.WriteToUDP(dg[:], ctx.Addr); err != nil {
+        return fmt.Errorf("error sending datagram: %v", err)
     }
 
     return nil
