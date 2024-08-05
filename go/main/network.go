2a57972a9c9cb4a50e0a196f6d43f7ebd470b3db
resilience-me <emailsareobsolete@gmail.com>
2024-08-05 02:51:14 +0200

    Update network.go

--- go/main/network.go
+++ go/main/network.go
@@ -5,19 +5,19 @@ import (
     "net"
 )
 
-// SignAndSendResponse signs and sends the response datagram.
-func SignAndSendResponseDatagram(rd *ResponseDatagram, addr *net.UDPAddr, conn *net.UDPConn, dir string) error {
-    // Call generateSignature directly with the ResponseDatagram's byte representation
-    signature, err := GenerateSignature((*rd)[:], dir)
+// SignAndSendResponseDatagram signs and sends the response datagram.
+func SignAndSendResponseDatagram(responseDg *ResponseDatagram, addr *net.UDPAddr, conn *net.UDPConn, dir string) error {
+    // Generate signature for ResponseDatagram
+    signature, err := GenerateSignature((*responseDg)[:], dir)
     if err != nil {
         return fmt.Errorf("failed to generate signature for ResponseDatagram: %w", err)
     }
 
     // Copy the generated signature into the response datagram's signature field
-    copy(rd.Signature[:], signature)
+    copy(responseDg.Signature[:], signature)
 
     // Send the signed response datagram
-    _, err := conn.WriteToUDP(rd[:], addr)
+    _, err = conn.WriteToUDP(responseDg[:], addr)
     if err != nil {
         return fmt.Errorf("error sending response datagram: %w", err)
     }
@@ -25,9 +25,9 @@ func SignAndSendResponseDatagram(rd *ResponseDatagram, addr *net.UDPAddr, conn *
     return nil
 }
 
-// SignAndSendResponse signs and sends the response datagram.
+// SignAndSendDatagram signs and sends the datagram
 func SignAndSendDatagram(dg *Datagram, addr *net.UDPAddr, conn *net.UDPConn, dir string) error {
-    // Call generateSignature directly with the Datagram's byte representation
+    // Generate signature for Datagram
     signature, err := GenerateSignature((*dg)[:], dir)
     if err != nil {
         return fmt.Errorf("failed to generate signature for Datagram: %w", err)
@@ -36,10 +36,10 @@ func SignAndSendDatagram(dg *Datagram, addr *net.UDPAddr, conn *net.UDPConn, dir
     // Copy the generated signature into the datagram's signature field
     copy(dg.Signature[:], signature)
 
-    // Send the signed response datagram
+    // Send the signed datagram
     _, err := conn.WriteToUDP(dg[:], addr)
     if err != nil {
-        return fmt.Errorf("error sending response datagram: %w", err)
+        return fmt.Errorf("error sending datagram: %w", err)
     }
 
     return nil
