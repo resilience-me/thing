85bf16e56613406d8ed659c6399efc2e85855de5
resilience-me <emailsareobsolete@gmail.com>
2024-08-05 07:33:24 +0200

    Update client_responses.go

--- go/handlers/client_responses.go
+++ go/handlers/client_responses.go
@@ -6,7 +6,7 @@ import (
 )
 
 // sendErrorResponse prepares and sends an error response datagram.
-func sendErrorResponse(ctx main.HandlerContext, errorMessage string) error {
+func SendErrorResponse(ctx main.HandlerContext, errorMessage string) error {
     var responseDg main.ResponseDatagram
     // Use the original signature as the nonce, dereferencing ctx.Datagram
     copy(responseDg.Nonce[:], ctx.Datagram.Signature[:]) 
@@ -23,6 +23,26 @@ func sendErrorResponse(ctx main.HandlerContext, errorMessage string) error {
     }
 
     // Send the signed response datagram
+    _, err := ctx.Conn.WriteToUDP(responseDg[:], ctx.Addr)
+    if err != nil {
+        return fmt.Errorf("error sending response datagram: %w", err)
+    }
+
+    return nil
+}
+
+func SendSuccessResponse(ctx main.HandlerContext, result []byte) error {
+    var responseDg main.ResponseDatagram
+    copy(responseDg.Nonce[:], ctx.Datagram.Signature[:])
+    responseDg.Result[0] = 0 // Set success code
+    copy(responseDg.Result[1:], result)
+
+    accountDir := main.GetAccountDir(ctx.Datagram)
+    if err := main.SignResponseDatagram(&responseDg, accountDir); err != nil {
+        fmt.Printf("Failed to sign response datagram: %v\n", err)
+        return err
+    }
+
     _, err := ctx.Conn.WriteToUDP(responseDg[:], ctx.Addr)
     if err != nil {
         return fmt.Errorf("error sending response datagram: %w", err)
