d2b4567310868f09619f6b166803ee1efc42a1bc
resilience-me <emailsareobsolete@gmail.com>
2024-08-05 05:00:43 +0200

    Update database.go

--- go/main/database.go
+++ go/main/database.go
@@ -9,7 +9,7 @@ import (
 var datadir = filepath.Join(os.Getenv("HOME"), "resilience")
 
 // GetAccountDir constructs the account directory path from the datagram
-func GetAccountDir(dg Datagram) (string, error) {
+func GetAccountDir(dg *Datagram) (string, error) { // Change to pointer
     username := string(dg.XUsername[:])
     accountDir := filepath.Join(datadir, "accounts", username)
 
@@ -22,7 +22,7 @@ func GetAccountDir(dg Datagram) (string, error) {
 }
 
 // GetPeerDir constructs the peer directory path from the datagram and checks if it exists.
-func GetPeerDir(dg Datagram, accountDir string) (string, error) {
+func GetPeerDir(dg *Datagram, accountDir string) (string, error) { // Change to pointer
     peerUsername := string(dg.YUsername[:])
     peerAddress := string(dg.YServerAddress[:])
 
