74e1162b97dd028d5c77405e0b26a3137b4583ff
resilience-me <emailsareobsolete@gmail.com>
2024-08-06 04:05:36 +0200

    Update write.go

--- go/database/db_trustlines/write.go
+++ go/database/db_trustlines/write.go
@@ -20,42 +20,42 @@ func writeTimeToFile(dir, filename string, timestamp int64) error {
 
 // SetTrustlineOut sets the outbound trustline amount.
 func SetTrustlineOut(dg *Datagram, value uint32) error {
-	trustlineDir := GetTrustlineDir(dg)
-	return writeUint32ToFile(trustlineDir, "trustline_out.txt", value)
+	trustlineDir := database.GetTrustlineDir(dg)
+	return database.WriteUint32ToFile(trustlineDir, "trustline_out.txt", value)
 }
 
 // SetCounter sets the counter value.
 func SetCounter(dg *Datagram, value uint32) error {
-	trustlineDir := GetTrustlineDir(dg)
-	return writeUint32ToFile(trustlineDir, "counter.txt", value)
+	trustlineDir := database.GetTrustlineDir(dg)
+	return database.WriteUint32ToFile(trustlineDir, "counter.txt", value)
 }
 
 // SetSyncIn sets the sync_in value.
 func SetSyncIn(dg *Datagram, value uint32) error {
-	trustlineDir := GetTrustlineDir(dg)
-	return writeUint32ToFile(trustlineDir, "sync_in.txt", value)
+	trustlineDir := database.GetTrustlineDir(dg)
+	return database.WriteUint32ToFile(trustlineDir, "sync_in.txt", value)
 }
 
 // SetSyncOut sets the sync_out value.
 func SetSyncOut(dg *Datagram, value uint32) error {
-	trustlineDir := GetTrustlineDir(dg)
-	return writeUint32ToFile(trustlineDir, "sync_out.txt", value)
+	trustlineDir := database.GetTrustlineDir(dg)
+	return database.WriteUint32ToFile(trustlineDir, "sync_out.txt", value)
 }
 
 // SetSyncCounterOut sets the sync_counter_out value.
 func SetSyncCounterOut(dg *Datagram, value uint32) error {
-	trustlineDir := GetTrustlineDir(dg)
-	return writeUint32ToFile(trustlineDir, "sync_counter_out.txt", value)
+	trustlineDir := database.GetTrustlineDir(dg)
+	return database.WriteUint32ToFile(trustlineDir, "sync_counter_out.txt", value)
 }
 
 // SetSyncCounterIn sets the sync_counter_in value.
 func SetSyncCounterIn(dg *Datagram, value uint32) error {
-	trustlineDir := GetTrustlineDir(dg)
-	return writeUint32ToFile(trustlineDir, "sync_counter_in.txt", value)
+	trustlineDir := database.GetTrustlineDir(dg)
+	return database.WriteUint32ToFile(trustlineDir, "sync_counter_in.txt", value)
 }
 
 // SetTimestamp sets the sync timestamp.
 func SetTimestamp(dg *Datagram, timestamp int64) error {
-	trustlineDir := GetTrustlineDir(dg)
-	return writeTimeToFile(trustlineDir, "timestamp.txt", timestamp)
+	trustlineDir := database.GetTrustlineDir(dg)
+	return database.WriteTimeToFile(trustlineDir, "timestamp.txt", timestamp)
 }
