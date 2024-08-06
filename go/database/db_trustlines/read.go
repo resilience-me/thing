72c2674f40a7376b9b306df6e92d8dc496d6f56a
resilience-me <emailsareobsolete@gmail.com>
2024-08-06 04:01:51 +0200

    Update read.go

--- go/database/db_trustlines/read.go
+++ go/database/db_trustlines/read.go
@@ -5,62 +5,48 @@ import (
 	"os"
 	"path/filepath"
 	"strconv"
+
+	"resilience/database"
 )
-
-// getUint32FromFile reads the contents of a file, parses it as a uint32, and returns the value.
-func getUint32FromFile(dir, filename string) (uint32, error) {
-	filePath := filepath.Join(dir, filename)
-	data, err := os.ReadFile(filePath)
-	if err != nil {
-		return 0, fmt.Errorf("error reading file %s: %v", filePath, err)
-	}
-
-	// Convert the file content to uint32
-	value, err := strconv.ParseUint(string(data), 10, 32)
-	if err != nil {
-		return 0, fmt.Errorf("error parsing value from file %s: %v", filePath, err)
-	}
-	return uint32(value), nil
-}
 
 // GetTrustlineOut retrieves the outbound trustline using the datagram to determine the directory.
 func GetTrustlineOut(dg *Datagram) (uint32, error) {
-	trustlineDir := GetTrustlineDir(dg)
-	return getUint32FromFile(trustlineDir, "trustline_out.txt")
+	trustlineDir := database.GetTrustlineDir(dg)
+	return database.GetUint32FromFile(trustlineDir, "trustline_out.txt")
 }
 
 // GetTrustlineIn retrieves the inbound trustline using the datagram to determine the directory.
 func GetTrustlineIn(dg *Datagram) (uint32, error) {
-	trustlineDir := GetTrustlineDir(dg)
-	return getUint32FromFile(trustlineDir, "trustline_in.txt")
+	trustlineDir := database.GetTrustlineDir(dg)
+	return database.GetUint32FromFile(trustlineDir, "trustline_in.txt")
 }
 
 // GetCounter retrieves the counter value using the datagram to determine the directory.
 func GetCounter(dg *Datagram) (uint32, error) {
-	trustlineDir := GetTrustlineDir(dg)
-	return getUint32FromFile(trustlineDir, "counter.txt")
+	trustlineDir := database.GetTrustlineDir(dg)
+	return database.GetUint32FromFile(trustlineDir, "counter.txt")
 }
 
 // GetSyncIn retrieves the sync_in value using the datagram to determine the directory.
 func GetSyncIn(dg *Datagram) (uint32, error) {
-	trustlineDir := GetTrustlineDir(dg)
-	return getUint32FromFile(trustlineDir, "sync_in.txt")
+	trustlineDir := database.GetTrustlineDir(dg)
+	return database.GetUint32FromFile(trustlineDir, "sync_in.txt")
 }
 
 // GetSyncOut retrieves the sync_out value using the datagram to determine the directory.
 func GetSyncOut(dg *Datagram) (uint32, error) {
-	trustlineDir := GetTrustlineDir(dg)
-	return getUint32FromFile(trustlineDir, "sync_out.txt")
+	trustlineDir := database.GetTrustlineDir(dg)
+	return database.GetUint32FromFile(trustlineDir, "sync_out.txt")
 }
 
 // GetSyncCounterIn retrieves the sync_counter_in value using the datagram to determine the directory.
 func GetSyncCounterIn(dg *Datagram) (uint32, error) {
-	trustlineDir := GetTrustlineDir(dg)
-	return getUint32FromFile(trustlineDir, "sync_counter_in.txt")
+	trustlineDir := database.GetTrustlineDir(dg)
+	return database.GetUint32FromFile(trustlineDir, "sync_counter_in.txt")
 }
 
 // GetSyncCounterOut retrieves the sync_counter_out value using the datagram to determine the directory.
 func GetSyncCounterOut(dg *Datagram) (uint32, error) {
-	trustlineDir := GetTrustlineDir(dg)
-	return getUint32FromFile(trustlineDir, "sync_counter_out.txt")
+	trustlineDir := database.GetTrustlineDir(dg)
+	return database.GetUint32FromFile(trustlineDir, "sync_counter_out.txt")
 }
