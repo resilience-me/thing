c5ef332eb8933d39f9d91ff7618334acf4b4b59c
resilience-me <emailsareobsolete@gmail.com>
2024-08-06 03:52:32 +0200

    Update protocol.go

--- go/main/protocol.go
+++ go/main/protocol.go
@@ -36,15 +36,19 @@ type CommandHandler func(HandlerContext)
 // CommandHandlers holds the command handlers
 var commandHandlers = [256]CommandHandler{
     0:   client_trustlines.SetTrustline,    // Client Command
+    1:   client_trustlines.GetTrustline,    // Client Command
     128: client_trustlines.SetTrustline,    // Server Command
-    129: server_trustlines.SetSyncCounter,  // Server Command
-    130: server_trustlines.GetTrustline,    // Server Command
+    129: server_trustlines.GetTrustline,    // Server Command
+    130: server_trustlines.SetSyncOut,      // Server Command
+    131: server_trustlines.SetTimestamp,    // Server Command
     // Other indices are nil by default
 }
 
 const (
     ClientTrustlines_SetTrustline   = 0
+    ClientTrustlines_GetTrustline   = 1
     ServerTrustlines_SetTrustline   = 128
-    ServerTrustlines_SetSyncCounter = 129
-    ServerTrustlines_GetTrustline   = 130
+    ServerTrustlines_GetTrustline   = 129
+    ServerTrustlines_SetSyncOut     = 130
+    ServerTrustlines_SetTimestamp   = 131
 )
