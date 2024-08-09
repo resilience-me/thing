f5bcedc0a9f7e9fe9124e9d5dfd3cb256959b089
resilience-me <emailsareobsolete@gmail.com>
2024-08-09 15:31:22 +0200

    Update types.go

--- go1/types.go
+++ go1/types.go
@@ -1,5 +1,5 @@
 
-// ToString converts a [32]byte array to a string, trimming trailing zeroes.
+// ToString converts a byte slice to a string, trimming trailing zeroes.
 func ToString(b []byte) string {
     return string(bytes.TrimRight(b, "\x00"))
 }
