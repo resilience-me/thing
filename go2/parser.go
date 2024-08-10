cda9dc00e1fe45bd3d9880e1bd6860fd25cf096d
resilience-me <emailsareobsolete@gmail.com>
2024-08-10 14:07:05 +0200

    Update parser.go

--- go2/parser.go
+++ go2/parser.go
@@ -14,6 +14,20 @@ type DatagramParser struct {
     Ciphertext []byte
 }
 
+// parseTransaction parses the decrypted plaintext into a Transaction struct
+func (dp *DatagramParser) parseTransaction(plaintext []byte) (*Transaction, error) {
+    tx := &Transaction{
+        Command:           plaintext[0],
+        Username:          string(plaintext[1:33]),  // Assuming Username is 32 bytes
+        PeerUsername:      string(plaintext[33:65]), // Assuming PeerUsername is 32 bytes
+        PeerServerAddress: string(plaintext[65:129]), // Assuming PeerServerAddress is 64 bytes
+    }
+    copy(tx.Arguments[:], plaintext[129:385]) // Copy Arguments (256 bytes)
+    copy(tx.Counter[:], plaintext[385:389])   // Copy Counter (4 bytes)
+
+    return tx, nil
+}
+
 // loadKey loads the cryptographic key based on the hash identifier in the datagram
 func (dp *DatagramParser) loadKey() ([]byte, error) {
     keyDirPath := filepath.Join(datadir, "keys", dp.Identifier)
@@ -47,22 +61,16 @@ func (dp *DatagramParser) decryptPayload(key []byte) ([]byte, error) {
     return plaintext, nil
 }
 
-// parseTransaction parses the decrypted plaintext into a Transaction struct
-func (dp *DatagramParser) parseTransaction(plaintext []byte) (*Transaction, error) {
-    tx := &Transaction{
-        Command:           plaintext[0],
-        Username:          string(plaintext[1:33]),  // Assuming Username is 32 bytes
-        PeerUsername:      string(plaintext[33:65]), // Assuming PeerUsername is 32 bytes
-        PeerServerAddress: string(plaintext[65:129]), // Assuming PeerServerAddress is 64 bytes
-    }
-    copy(tx.Arguments[:], plaintext[129:385]) // Copy Arguments (256 bytes)
-    copy(tx.Counter[:], plaintext[385:389])   // Copy Counter (4 bytes)
-
-    return tx, nil
-}
-
 // decryptAndParseDatagram decrypts and parses the datagram into a Transaction
-func (dp *DatagramParser) decryptAndParseDatagram(buf []byte) (*Transaction, error) {
+func decryptAndParseDatagram(buf []byte) (*Transaction, error) {
+
+    // Create a DatagramParser instance directly, inlining the string conversion
+    dp := DatagramParser{
+        Identifier: string(buf[:32]),   // Convert the identifier part to a string
+        Salt:       buf[32:44],         // The salt part (12 bytes)
+        Ciphertext: buf[44:],           // The rest is the ciphertext
+    }
+
     // Load the cryptographic key based on the identifier in the datagram
     secretKey, err := dp.loadKey()
     if err != nil {
