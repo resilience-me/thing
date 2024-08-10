7e7f25e636e7bdee792848e447a3fe52cb522f7e
resilience-me <emailsareobsolete@gmail.com>
2024-08-10 18:06:34 +0200

    Update protocol.go

--- go3/protocol.go
+++ go3/protocol.go
@@ -1,12 +1,12 @@
 // Datagram holds the structure of the incoming data
 type Datagram struct {
-    Command        byte
-    XUsername      [32]byte
-    YUsername      [32]byte
-    YServerAddress [32]byte
-    Arguments      [256]byte
-    Counter        [4]byte
-    Signature      [32]byte
+    Command           byte
+    Username          string
+    PeerUsername      string
+    PeerServerAddress string
+    Arguments         [256]byte
+    Counter           [4]byte
+    Signature         [32]byte
 }
 
 // CommandHandler defines the type for command handling functions
