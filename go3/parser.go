564a56e0641698e7218360c4c62f391dcef5f64d
resilience-me <emailsareobsolete@gmail.com>
2024-08-11 09:06:23 +0200

    Update parser.go

--- go3/parser.go
+++ go3/parser.go
@@ -3,7 +3,26 @@ package main
 import (
     "bytes"        // For trimming null characters from byte slices
     "encoding/binary"
+    "ripple/database"
 )
+
+// CheckUserAndPeerExist checks for the existence of user and peer directories.
+// It returns an appropriate error code and an error object for detailed information if an error occurs.
+func checkUserAndPeerExist(dg *Datagram) (byte, error) {
+    if exists, err := database.CheckAccountExists(dg); err != nil {
+        return ErrCheckExistence, fmt.Errorf("error checking account existence for user '%s': %v", dg.Username, err)
+    } else if !exists {
+        return ErrAccountNotExist, fmt.Errorf("account directory does not exist for user '%s'", dg.Username)
+    }
+
+    if exists, err = database.CheckPeerExists(dg); err != nil {
+        return ErrCheckExistence, fmt.Errorf("error checking peer existence for server '%s' and user '%s': %v", dg.PeerServerAddress, dg.PeerUsername, err)
+    } else if !exists {
+        return ErrPeerNotExist, fmt.Errorf("peer directory does not exist for server '%s' and user '%s'", dg.PeerServerAddress, dg.PeerUsername)
+    }
+
+    return 0, nil // No error, directories exist
+}
 
 func parseDatagram(buf []byte) *Datagram {
     // Assuming buf is already confirmed to be of the correct length via io.ReadFull
