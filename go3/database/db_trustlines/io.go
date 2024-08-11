e8655cfc47320fc6bf7e717112c604b3719728cb
resilience-me <emailsareobsolete@gmail.com>
2024-08-11 18:29:49 +0200

    Update io.go

--- go3/database/db_trustlines/io.go
+++ go3/database/db_trustlines/io.go
@@ -5,16 +5,19 @@ import (
 )
 
 // GetAndIncrementCounterOut retrieves the current counter_out, increments it, and updates the database.
+// It returns the counter value before it was incremented.
 func GetAndIncrementCounterOut(datagram *main.Datagram) (uint32, error) {
+    // Retrieve the current value of counter_out from the database.
     counterOut, err := GetCounterOut(datagram)
     if err != nil {
-        return 0, err
+        return 0, err  // Return error if unable to fetch the counter.
     }
 
-    // Increment and update the counter_out in the database
-    if err := SetCounterOut(datagram, counterOut+1); err != nil {
-        return 0, err
+    // Increment the counter and update it in the database within the same function call.
+    if err := SetCounterOut(datagram, counterOut + 1); err != nil {
+        return 0, err  // Return error if unable to update the counter.
     }
 
+    // Return the original counter value that was fetched.
     return counterOut, nil
 }
