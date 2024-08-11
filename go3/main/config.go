4bc2e15a50959cf5440ef5a5a8d7aaa9819a5982
resilience-me <emailsareobsolete@gmail.com>
2024-08-11 16:03:23 +0200

    Create config.go

---
+++ go3/main/config.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+    "fmt"
+    "os"
+    "path/filepath"
+)
+
+var serverAddress string // Store the server address as a byte array
+
+// GetServerAddress returns the server address as a byte slice
+func GetServerAddress() string {
+    return serverAddress
+}
+
+// loadServerAddress reads the server address from the configuration file.
+func loadServerAddress() error {
+    addressPath := filepath.Join(datadir, "server_address.txt")
+    address, err := os.ReadFile(addressPath)
+    if err != nil {
+        return fmt.Errorf("error loading server address: %w", err)
+    }
+    serverAddress = string(address)
+    return nil
+}
+
+// InitConfig initializes the configuration
+func initConfig() error {
+    if err := loadServerAddress(); err != nil {
+        return err
+    }
+    return nil
+}
