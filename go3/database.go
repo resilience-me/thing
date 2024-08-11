2a90992bb5c871605fe7f35f9b2feb68815dd733
resilience-me <emailsareobsolete@gmail.com>
2024-08-11 08:17:09 +0200

    Update database.go

--- go3/database.go
+++ go3/database.go
@@ -7,6 +7,23 @@ import (
 
 var datadir = filepath.Join(os.Getenv("HOME"), "ripple")
 
+// GetAccountDir constructs the account directory path from the datagram
+func GetAccountDir(dg *Datagram) string {
+    return filepath.Join(datadir, "accounts", dg.Username)
+}
+
+// GetPeerDir constructs the peer directory path from the datagram and returns it
+func GetPeerDir(dg *Datagram) string {
+    accountDir := GetAccountDir(dg)
+    return filepath.Join(accountDir, "peers", dg.PeerServerAddress, dg.PeerUsername)
+}
+
+// GetTrustlineDir constructs the trustline directory path from the datagram and returns it.
+func GetTrustlineDir(dg *Datagram) string {
+    peerDir := GetPeerDir(dg)
+    return filepath.Join(peerDir, "trustline")
+}
+
 // checkDirExists checks if a specific directory exists.
 func checkDirExists(dirPath string) (bool, error) {
     // Use os.Stat to attempt to retrieve the directory information
@@ -20,4 +37,18 @@ func checkDirExists(dirPath string) (bool, error) {
     }
     // The directory exists
     return true, nil
+}
+
+// CheckAccountExists checks if the account directory exists
+func CheckAccountExists(dg *Datagram) (bool, error) {
+    accountDir := GetAccountDir(dg)
+    // Ensure the account directory exists
+    return checkDirExists(accountDir)
+}
+
+// CheckPeerExists checks if the peer directory exists
+func CheckPeerExists(dg *Datagram) (bool, error) {
+    peerDir := GetPeerDir(dg)
+    // Ensure the peer directory exists
+    return checkDirExists(peerDir)
 }
