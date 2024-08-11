f8ff035f015d3f37052a6b84049549eb6b984b81
resilience-me <emailsareobsolete@gmail.com>
2024-08-11 13:25:27 +0200

    Update types.go

--- go3/main/types.go
+++ go3/main/types.go
@@ -1,3 +1,9 @@
+package main
+
+import (
+    "encoding/binary"
+)
+
 // uint32ToBytes converts a uint32 value to a byte slice.
 func uint32ToBytes(value uint32) []byte {
     responseData := make([]byte, 4) // Allocate 4 bytes for the uint32 value
