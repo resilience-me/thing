999a2a524920f8e55d1e38e77e3bca622236e3f8
resilience-me <emailsareobsolete@gmail.com>
2024-08-11 14:08:11 +0200

    Update setTrustline.go

--- go3/handlers/server_trustlines/setTrustline.go
+++ go3/handlers/server_trustlines/setTrustline.go
@@ -35,7 +35,8 @@ func SetTrustline(session main.Session) {
     }
 
     // Retrieve the syncIn counter from the Datagram
-    syncIn := main.BytesToUint32(session.Datagram.Arguments[4:8])
+    syncInBytes := session.Datagram.Arguments[4:8]
+    syncIn := main.BytesToUint32(syncInBytes)
     if syncIn <= prevSyncIn {
         fmt.Println("The local sync_in value is synchronized with the peers most recent trustline_out update.")
     } else {
@@ -61,13 +62,22 @@ func SetTrustline(session main.Session) {
     }
 
     if syncIn > prevSyncIn {
+
+        // Retrieve the counter_in value using the new getter
+        counterOut, err := db_trustlines.GetCounterOut(session.Datagram)
+        if err != nil {
+            fmt.Printf("Error getting counter_out: %v\n", err)
+            return
+        }
+
         // Prepare the datagram to send back to the peer
         dg := main.Datagram{
             Command:        main.ServerTrustlines_SetSyncOut,
             Username:      session.Datagram.PeerUsername,       // Reverse the usernames for response
             PeerUsername:      session.Datagram.Username,
             PeerServerAddress: main.GetServerAddress(),      // Use the server's address
-            Counter:        counter,                        // Directly using the uint32 counter
+            Arguments:     syncInBytes,
+            Counter:        counterOut,
         }
     
         // Replace explicit signing and sending with the centralized function call
