c5d420a424b9ef9725160ecd639ce5dffd8bd2da
resilience-me <emailsareobsolete@gmail.com>
2024-08-11 17:19:15 +0200

    Update setTrustline.go

--- go3/handlers/server_trustlines/setTrustline.go
+++ go3/handlers/server_trustlines/setTrustline.go
@@ -1,7 +1,7 @@
 package server_trustlines
 
 import (
-    "fmt"
+    "log"
     "time"
     "ripple/main"
     "ripple/handlers"
@@ -13,14 +13,14 @@ func SetTrustline(session main.Session) {
     // Retrieve the counter_in value using the new getter
     counterIn, err := db_trustlines.GetCounterIn(session.Datagram)
     if err != nil {
-        fmt.Printf("Error getting counter_in: %v\n", err)
+        log.Printf("Error getting counter_in for user %s: %v", session.Datagram.Username, err)
         return
     }
 
     // Check the counter
-    counter := session.Datagram.Counter // Directly using the uint32 counter
+    counter := session.Datagram.Counter
     if counter <= counterIn {
-        fmt.Println("Received counter is not greater than counter_in. Potential replay attack.")
+        log.Printf("Potential replay attack: received counter (%d) is not greater than counter_in (%d) for user %s.", counter, counterIn, session.Datagram.Username)
         return
     }
 
@@ -30,7 +30,7 @@ func SetTrustline(session main.Session) {
     // Retrieve the sync_in value using the new getter
     prevSyncIn, err := db_trustlines.GetSyncIn(session.Datagram)
     if err != nil {
-        fmt.Printf("Error getting sync_in: %v\n", err)
+        log.Printf("Error getting sync_in for user %s: %v", session.Datagram.Username, err)
         return
     }
 
@@ -39,56 +39,61 @@ func SetTrustline(session main.Session) {
     syncIn := main.BytesToUint32(syncInBytes)
 
     if syncIn > prevSyncIn {
-        // Write the new trustline amount using the setter
-        if err := db_trustlines.SetTrustlineIn(session.Datagram, trustlineAmount); err != nil {
-            fmt.Printf("Error writing trustline to file: %v\n", err)
-            return
-        }
-    
-        // Write the new sync_in value using the setter
-        if err := db_trustlines.SetSyncIn(session.Datagram, syncIn); err != nil {
-            fmt.Printf("Error writing sync_in to file: %v\n", err)
-            return
-        }
-    
-        // Write the Unix timestamp using the setter
-        if err := db_trustlines.SetTimestamp(session.Datagram, time.Now().Unix()); err != nil {
-            fmt.Printf("Error writing timestamp to file: %v\n", err)
-            return
-        }
-        fmt.Println("trustline_in, sync_in and timestamp updated successfully.")
-
-        // Retrieve the counter_out value using the getter
-        counterOut, err := db_trustlines.GetCounterOut(session.Datagram)
-        if err != nil {
-            fmt.Printf("Error getting counter_out: %v\n", err)
-            return
-        }
-
-        // Prepare the datagram to send back to the peer
-        dg := main.Datagram{
-            Command:           main.ServerTrustlines_SetSyncOut,
-            Username:          session.Datagram.PeerUsername,
-            PeerUsername:      session.Datagram.Username,
-            PeerServerAddress: main.GetServerAddress(),      // Use the server's address
-            Arguments:         syncInBytes,
-            Counter:           counterOut,
-        }
-    
-        if err := handlers.SignAndSendDatagram(session, &dg); err != nil {
-            fmt.Printf("Failed to sign and send datagram: %v\n", err)
-            return
-        }
-
-        // Add a success message indicating all operations were successful
-        fmt.Println("Trustline update and datagram sending completed successfully.")
+        handleTrustlineUpdate(session, trustlineAmount, syncInBytes, syncIn)
     } else {
-        fmt.Println("The local sync_in value is synchronized with the peers most recent trustline_out update.")
-        // Write the Unix timestamp using the setter
-        if err := db_trustlines.SetTimestamp(session.Datagram, time.Now().Unix()); err != nil {
-            fmt.Printf("Error writing timestamp to file: %v\n", err)
-            return
-        }
-        fmt.Println("The local trustline_in synchronization timestamp updated successfully.")
-    } 
+        handleSyncOnly(session, syncInBytes)
+    }
+}
+
+func handleTrustlineUpdate(session main.Session, trustlineAmount uint32, syncInBytes []byte, syncIn uint32) {
+    if err := db_trustlines.SetTrustlineIn(session.Datagram, trustlineAmount); err != nil {
+        log.Printf("Error writing trustline to file for user %s: %v", session.Datagram.Username, err)
+        return
+    }
+
+    if err := db_trustlines.SetSyncIn(session.Datagram, syncIn); err != nil {
+        log.Printf("Error writing sync_in to file for user %s: %v", session.Datagram.Username, err)
+        return
+    }
+
+    if err := db_trustlines.SetTimestamp(session.Datagram, time.Now().Unix()); err != nil {
+        log.Printf("Error writing timestamp to file for user %s: %v", session.Datagram.Username, err)
+        return
+    }
+    log.Printf("Trustline, sync_in, and timestamp updated successfully for user %s.", session.Datagram.Username)
+
+    // Retrieve the counter_out value
+    counterOut, err := db_trustlines.GetCounterOut(session.Datagram)
+    if err != nil {
+        log.Printf("Error getting counter_out for user %s: %v", session.Datagram.Username, err)
+        return
+    }
+
+    // Prepare the datagram to send back to the peer
+    dg := main.Datagram{
+        Command:           main.ServerTrustlines_SetSyncOut,
+        Username:          session.Datagram.PeerUsername,
+        PeerUsername:      session.Datagram.Username,
+        PeerServerAddress: main.GetServerAddress(),
+        Arguments:         syncInBytes,
+        Counter:           counterOut,
+    }
+
+    if err := handlers.SignAndSendDatagram(session, &dg); err != nil {
+        log.Printf("Failed to sign and send datagram for user %s: %v", session.Datagram.Username, err)
+        return
+    }
+
+    log.Printf("Trustline update and datagram sent successfully for user %s.", session.Datagram.Username)
+}
+
+func handleSyncOnly(session main.Session, syncInBytes []byte) {
+    log.Printf("Sync_in is synchronized with the peer's most recent trustline_out for user %s.", session.Datagram.Username)
+
+    if err := db_trustlines.SetTimestamp(session.Datagram, time.Now().Unix()); err != nil {
+        log.Printf("Error writing timestamp to file for user %s: %v", session.Datagram.Username, err)
+        return
+    }
+
+    log.Printf("Trustline synchronization timestamp updated successfully for user %s.", session.Datagram.Username)
 }
