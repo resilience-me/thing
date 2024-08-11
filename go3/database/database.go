509e01f7c2385bac7011075701db8e9859f19843
resilience-me <emailsareobsolete@gmail.com>
2024-08-11 08:36:14 +0200

    Update database.go

--- go3/database/database.go
+++ go3/database/database.go
@@ -54,7 +54,7 @@ func CheckPeerExists(dg *Datagram) (bool, error) {
 }
 
 // checkUserAndPeerExist checks if both account and peer directories exist for the given datagram.
-func checkUserAndPeerExist(dg *Datagram) (bool, error) {
+func CheckUserAndPeerExist(dg *Datagram) (bool, error) {
     // Check if account directory exists
     if exists, err := CheckAccountExists(dg); err != nil {
         return false, fmt.Errorf("error checking account existence for user '%s': %v", dg.Username, err)
