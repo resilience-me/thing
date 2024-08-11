d6b05b311e704a476e65b45fb8969fffffb7fc0a
resilience-me <emailsareobsolete@gmail.com>
2024-08-11 13:25:11 +0200

    Create types.go

---
+++ go3/main/types.go
@@ -0,0 +1,6 @@
+// uint32ToBytes converts a uint32 value to a byte slice.
+func uint32ToBytes(value uint32) []byte {
+    responseData := make([]byte, 4) // Allocate 4 bytes for the uint32 value
+    binary.BigEndian.PutUint32(responseData, value) // Convert the uint32 to bytes
+    return responseData
+}
