6c3c5a79b0408118f590e3bf4fbf90cff3414e6b
resilience-me <emailsareobsolete@gmail.com>
2024-08-11 20:15:12 +0200

    Update io.go

--- go3/database/db_trustlines/io.go
+++ go3/database/db_trustlines/io.go
@@ -21,3 +21,21 @@ func GetAndIncrementCounterOut(datagram *main.Datagram) (uint32, error) {
     // Return the original counter value that was fetched.
     return counterOut, nil
 }
+
+// IncrementSyncCounter retrieves the current sync_counter, increments it, and updates the database.
+// It returns an error if something goes wrong during the process.
+func IncrementSyncCounter(datagram *main.Datagram) error {
+    // Retrieve the current value of sync_counter from the database.
+    syncCounter, err := GetSyncCounter(datagram)
+    if err != nil {
+        return err  // Return error if unable to fetch the sync_counter.
+    }
+
+    // Increment the counter and update it in the database within the same function call.
+    if err := SetSyncCounter(datagram, syncCounter + 1); err != nil {
+        return err  // Return error if unable to update the sync_counter.
+    }
+
+    // No need to return any value; just indicate success by returning nil.
+    return nil
+}
