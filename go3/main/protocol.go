cd628908136dcd79ee9b9f4f0bdbdf7d5f7a65c0
resilience-me <emailsareobsolete@gmail.com>
2024-08-11 16:50:23 +0200

    Update protocol.go

--- go3/main/protocol.go
+++ go3/main/protocol.go
@@ -1,5 +1,12 @@
 package main
 
+package main
+
+import (
+    "ripple/handlers/client_trustlines"
+    "ripple/handlers/server_trustlines"
+)
+
 // Datagram holds the structure of the incoming data
 type Datagram struct {
     Command           byte
@@ -16,9 +23,22 @@ type CommandHandler func(session Session)
 
 // CommandHandlers maps command bytes to handler functions
 var commandHandlers = [256]CommandHandler{
-    0: handleClientCommand1,
-    1: handleClientCommand2,
-    127: handleServerCommand1,
-    128: handleServerCommand2,
-    // Add more command handlers as needed
+    0:   client_trustlines.SetTrustline,    // Client Command
+    1:   client_trustlines.GetTrustlineOut, // Client Command
+    2:   client_trustlines.GetTrustlineIn,  // Client Command
+    128: client_trustlines.SetTrustline,    // Server Command
+    129: server_trustlines.GetTrustline,    // Server Command
+    130: server_trustlines.SetSyncOut,      // Server Command
+    131: server_trustlines.SetTimestamp,    // Server Command
+    // Other indices are nil by default
 }
+
+const (
+    ClientTrustlines_SetTrustline      = 0
+    ClientTrustlines_GetTrustlineOut   = 1
+    ClientTrustlines_GetTrustlineIn    = 2
+    ServerTrustlines_SetTrustline      = 128
+    ServerTrustlines_GetTrustline      = 129
+    ServerTrustlines_SetSyncOut        = 130
+    ServerTrustlines_SetTimestamp      = 131
+)
