1c3397318cc9c45c40bfe82dd15c7a202666b090
resilience-me <emailsareobsolete@gmail.com>
2024-08-11 23:02:02 +0200

    Update transaction.go

--- go4/main/transaction.go
+++ go4/main/transaction.go
@@ -2,8 +2,9 @@
 package main
 
 import (
-    "encoding/binary"
-    "encoding/json"
+	"encoding/binary"
+	"fmt"
+	"os"
 )
 
 type Transaction struct {
@@ -16,34 +17,21 @@ type Transaction struct {
     Signature         [64]byte // Digital signature using the validator's private key
 }
 
-// AppendTransaction appends a new transaction to the JSON file
-func appendTransaction(accountUsername, peerUsername string, transaction Transaction) error {
-    relationshipFilePath := filepath.Join("data", "accounts", accountUsername, "peers", peerUsername, "relationship.json")
+// AppendTransaction appends a new transaction to the specified file
+func AppendTransaction(filePath string, tx Transaction) error {
+	// Open the file in append mode, create if it doesn't exist
+	file, err := os.OpenFile(filePath, os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
+	if err != nil {
+		return err
+	}
+	defer file.Close()
 
-    // Open the file for appending
-    file, err := os.OpenFile(relationshipFilePath, os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
-    if err != nil {
-        return err
-    }
-    defer file.Close()
+	// Write the transaction to the file
+	_, err = file.Write(tx[:]) // Directly write the entire struct as bytes
+	if err != nil {
+		return err
+	}
 
-    // Not the first entry; append a comma for JSON array
-    _, err := file.WriteString(",")
-    if err != nil {
-        return err
-    }
-
-    // Marshal the new transaction to JSON
-    transactionJSON, err := json.Marshal(transaction)
-    if err != nil {
-        return err
-    }
-
-    // Write the new transaction to the file
-    _, err = file.Write(transactionJSON)
-    if err != nil {
-        return err
-    }
-
-    return nil
+	return nil
 }
+
