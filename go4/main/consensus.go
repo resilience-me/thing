5c128d86ad1199507199eca93202b48a1552afac
resilience-me <emailsareobsolete@gmail.com>
2024-08-12 02:16:40 +0200

    Create consensus.go

---
+++ go4/main/consensus.go
@@ -0,0 +1,25 @@
+// HandleTransactionRequest processes a transaction request from the non-validator.
+func HandleTransactionRequest(filename string, request TransactionRequest, validatorID []byte) error {
+    // Ensure that this account is the current validator
+    ValidatedLatestBlock, err := IsValidator(filename, validatorID)
+    if err != nil {
+        return err
+    }
+    if ValidatedLatestBlock {
+        return fmt.Errorf("this account is not the current validator")
+    }
+
+    // Convert the request into a full transaction
+    rawTransaction, err := PrepareTransaction(filename, request, validatorID)
+    if err != nil {
+        return fmt.Errorf("failed to prepare transaction: %v", err)
+    }
+
+    // Validate and append the transaction
+    err = SubmitTransaction(filename, rawTransaction, validatorID)
+    if err != nil {
+        return fmt.Errorf("failed to validate and add transaction: %v", err)
+    }
+
+    return nil
+}
