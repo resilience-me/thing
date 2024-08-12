9887b8f96c0c2fdf5c5dce70673d89718e77a53a
resilience-me <emailsareobsolete@gmail.com>
2024-08-12 04:16:03 +0200

    Update types.go

--- go4/main/types.go
+++ go4/main/types.go
@@ -1,12 +1,11 @@
 import (
 	"crypto/ecdsa"
+	"crypto/elliptic"
 	"crypto/sha256"
-	"fmt"
-	"math/big"
 )
 
 // GenerateAddress generates an address using SHA-256 from an ECDSA public key, skipping the prefix.
-func GenerateAddress(pubKey *ecdsa.PublicKey) string {
+func GenerateAddress(pubKey *ecdsa.PublicKey) []byte {
     // Get the uncompressed public key bytes
     pubKeyBytes := elliptic.Marshal(pubKey.Curve, pubKey.X, pubKey.Y)
 
@@ -14,8 +13,5 @@ func GenerateAddress(pubKey *ecdsa.PublicKey) string {
     hash := sha256.Sum256(pubKeyBytes[1:]) // Skip the 0x04 prefix
 
     // The address is the last 20 bytes of the hash
-    address := hash[len(hash)-20:]
-
-    // Return the address as a hexadecimal string prefixed with 0x
-    return fmt.Sprintf("0x%x", address)
+    return hash[len(hash)-20:]
 }
