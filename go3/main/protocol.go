360bc789ac04a4442fbe45eb0c00b8887ae6153b
resilience-me <emailsareobsolete@gmail.com>
2024-08-12 23:32:09 +0200

    Update protocol.go

--- go3/main/protocol.go
+++ go3/main/protocol.go
@@ -3,8 +3,8 @@ package main
 package main
 
 import (
-    "ripple/handlers/client_trustlines"
-    "ripple/handlers/server_trustlines"
+    "ripple/handlers/trustlines/client_trustlines"
+    "ripple/handlers/trustlines/server_trustlines"
 )
 
 // Datagram holds the structure of the incoming data
@@ -24,21 +24,25 @@ type CommandHandler func(session Session)
 // CommandHandlers maps command bytes to handler functions
 var commandHandlers = [256]CommandHandler{
     0:   client_trustlines.SetTrustline,    // Client Command
-    1:   client_trustlines.GetTrustlineOut, // Client Command
-    2:   client_trustlines.GetTrustlineIn,  // Client Command
-    128: client_trustlines.SetTrustline,    // Server Command
-    129: server_trustlines.GetTrustline,    // Server Command
-    130: server_trustlines.SetSyncOut,      // Server Command
+    1:   client_trustlines.SyncTrustlineIn, // Client Command
+    2:   client_trustlines.SyncTrustlineOut, // Client Command
+    3:   client_trustlines.GetTrustlineIn,  // Client Command
+    4:   client_trustlines.GetTrustlineOut, // Client Command
+    127: server_trustlines.SetTrustline,    // Server Command
+    128: server_trustlines.GetTrustline,    // Server Command
+    139: server_trustlines.SetSyncOut,      // Server Command
     131: server_trustlines.SetTimestamp,    // Server Command
     // Other indices are nil by default
 }
 
 const (
     ClientTrustlines_SetTrustline      = 0
-    ClientTrustlines_GetTrustlineOut   = 1
-    ClientTrustlines_GetTrustlineIn    = 2
-    ServerTrustlines_SetTrustline      = 128
-    ServerTrustlines_GetTrustline      = 129
-    ServerTrustlines_SetSyncOut        = 130
-    ServerTrustlines_SetTimestamp      = 131
+    ClientTrustlines_SyncTrustlineIn   = 1
+    ClientTrustlines_SyncTrustlineOut  = 2
+    ClientTrustlines_GetTrustlineIn    = 3
+    ClientTrustlines_GetTrustlineOut   = 4
+    ServerTrustlines_SetTrustline      = 127
+    ServerTrustlines_GetTrustline      = 128
+    ServerTrustlines_SetSyncOut        = 129
+    ServerTrustlines_SetTimestamp      = 130
 )
