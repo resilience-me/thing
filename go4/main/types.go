79f73fdb67f1a7ccb0bfb80d299c87aa20c9e11f
resilience-me <emailsareobsolete@gmail.com>
2024-08-12 04:31:22 +0200

    Update types.go

--- go4/main/types.go
+++ go4/main/types.go
@@ -14,14 +14,19 @@ type Transaction struct {
     Signature         [64]byte
 }
 
+func hashAndTruncateToAddress() []byte {
+    hash := sha256.Sum256(combined)
+    return hash[:20]  // Use the first 20 bytes of the hash
+}
 // GenerateAddress generates an address using SHA-256 from an ECDSA public key, skipping the prefix.
 func GenerateAddress(pubKey *ecdsa.PublicKey) []byte {
     // Get the uncompressed public key bytes
     pubKeyBytes := elliptic.Marshal(pubKey.Curve, pubKey.X, pubKey.Y)
-
-    // Compute the SHA-256 hash of the public key bytes, skipping the 0x04 prefix
-    hash := sha256.Sum256(pubKeyBytes[1:]) // Skip the 0x04 prefix
-
-    // The address is the last 20 bytes of the hash
-    return hash[len(hash)-20:]
+    // The address is the first 20 bytes of the hash
+    return hashAndTruncateToAddress(pubKeyBytes[1:])
+}
+// generateIdentifier creates a unique identifier using the first 20 bytes of a SHA-256 hash of the combined addresses.
+func generateIdentifier(myAddress, otherAddress []byte) []byte {
+    combined := append(myAddress, otherAddress...)
+    return hashAndTruncateToAddress(combined)
 }
