11f09ff7e1cc7e9f996f69b447782ac0fa1c23a9
resilience-me <emailsareobsolete@gmail.com>
2024-08-13 16:45:28 +0200

    Update types.go

--- go3/pathfinding/types.go
+++ go3/pathfinding/types.go
@@ -19,8 +19,15 @@ type PathNode struct {
     CounterOut         map[string]int
 }
 
-// AccountNode represents a node in the account linked list.
+// Payment struct to hold details about an ongoing payment.
+type Payment struct {
+    Identifier string // Unique identifier for the payment
+    InOrOut    bool   // True if outgoing (sender), false if incoming (receiver)
+}
+
+// AccountNode struct to represent each node's information in the account linked list.
 type AccountNode struct {
     linkedlist.BaseNode           // Embedding the base struct for shared fields
-    linkedlist.BaseList // Embed BaseList to manage the linked list of BaseNodes
+    linkedlist.BaseList           // Embed BaseList to manage the linked list of BaseNodes
+    Payment          *Payment     // Pointer to a Payment struct if a payment is active
 }
