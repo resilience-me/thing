b9c1daff1ea3232b4e4f031c003130dc1f9d729a
resilience-me <emailsareobsolete@gmail.com>
2024-08-13 17:35:03 +0200

    Update helpers.go

--- go3/pathfinding/helpers.go
+++ go3/pathfinding/helpers.go
@@ -1,5 +1,9 @@
 package pathfinding
 
+import (
+    "fmt"
+)
+
 // FindOrAdd looks for an existing account and returns it, or adds a new one if it does not exist.
 func (pm *PathManager) FindOrAdd(username string) *AccountNode {
     // Attempt to find the existing node
