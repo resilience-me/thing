487d8e65a85bb21e29806e675ef940908fb2e940
resilience-me <emailsareobsolete@gmail.com>
2024-08-13 17:59:11 +0200

    Update protocol.go

--- go3/main/protocol.go
+++ go3/main/protocol.go
@@ -5,7 +5,8 @@ package main
 import (
     "ripple/handlers/trustlines/client_trustlines"
     "ripple/handlers/trustlines/server_trustlines"
-    "ripple/handlers/payments"
+    "ripple/handlers/payments/client_payments"
+    "ripple/handlers/payments/server_payments"
 )
 
 // Datagram holds the structure of the incoming data
@@ -29,16 +30,16 @@ var commandHandlers = [256]CommandHandler{
     2:   client_trustlines.SyncTrustlineOut, // Client Command
     3:   client_trustlines.GetTrustlineIn,   // Client Command
     4:   client_trustlines.GetTrustlineOut,  // Client Command
+    5:   client_payments.NewPaymentOut,      // Client Command
+    6:   client_payments.NewPaymentIn,       // Client Command
     127: server_trustlines.SetTrustline,     // Server Command
     128: server_trustlines.GetTrustline,     // Server Command
     129: server_trustlines.SetSyncOut,       // Server Command
     130: server_trustlines.SetTimestamp,     // Server Command
-    131: payments.NewPaymentOut,             // Server Command
-    132: payments.NewPaymentIn,              // Server Command
-    133: payments.NewPathFindingOut,         // Server Command
-    134: payments.PathFindingOut,            // Server Command
-    135: payments.PathFindingIn,             // Server Command
-    136: payments.PathFindingRecurse,        // Server Command
+    131: server_payments.NewPathFindingOut,  // Server Command
+    132: server_payments.PathFindingOut,     // Server Command
+    133: server_payments.PathFindingIn,      // Server Command
+    134: server_payments.PathFindingRecurse, // Server Command
     // Other indices are nil by default
 }
 
@@ -48,14 +49,14 @@ const (
     ClientTrustlines_SyncTrustlineOut  = 2
     ClientTrustlines_GetTrustlineIn    = 3
     ClientTrustlines_GetTrustlineOut   = 4
+    ClientPayments_NewPaymentOut       = 5
+    ClientPayments_NewPaymentIn        = 6
     ServerTrustlines_SetTrustline      = 127
     ServerTrustlines_GetTrustline      = 128
     ServerTrustlines_SetSyncOut        = 129
     ServerTrustlines_SetTimestamp      = 130
-    Payments_NewPaymentOut             = 131
-    Payments_NewPaymentIn              = 132
-    Payments_NewPathFindingOut         = 133
-    Payments_PathFindingOut            = 134
-    Payments_PathFindingIn             = 135
-    Payments_PathFindingRecurse        = 136
+    ServerPayments_NewPathFindingOut   = 131
+    ServerPayments_PathFindingOut      = 132
+    ServerPayments_PathFindingIn       = 133
+    ServerPayments_PathFindingRecurse  = 134
 )
