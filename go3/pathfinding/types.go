f562e10c4ec70877ab716adf6948c0850d4aa319
resilience-me <emailsareobsolete@gmail.com>
2024-08-13 05:32:45 +0200

    Create types.go

---
+++ go3/pathfinding/types.go
@@ -0,0 +1,26 @@
+package pathfinding
+
+import (
+  "ripple/linkedlist"
+)
+
+// PeerAccount represents a peer in the network.
+type PeerAccount struct {
+    Username      string
+    ServerAddress string
+}
+
+// PathEntry represents an entry in the pathfinding linked list.
+type PathEntry struct {
+    LinkedListNodeBase // Embedding the base struct for shared fields
+    Incoming           PeerAccount
+    Outgoing           PeerAccount
+    CounterIn          int
+    CounterOut         map[string]int
+}
+
+// AccountNode represents a node in the account linked list.
+type AccountNode struct {
+    LinkedListNodeBase // Embedding the base struct for shared fields
+    PathFinding        *PathEntry // Linked list of PathEntry nodes
+}
