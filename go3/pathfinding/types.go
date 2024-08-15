f46115235b370e12870f9e6195a09718c548b452
resilience-me <emailsareobsolete@gmail.com>
2024-08-15 22:08:52 +0200

    Update types.go

--- go3/pathfinding/types.go
+++ go3/pathfinding/types.go
@@ -5,25 +5,25 @@ import (
     "time"
 )
 
-// PeerAccount holds details about a peer account.
+// PeerAccount holds details about a peer account
 type PeerAccount struct {
     Username      string
     ServerAddress string
 }
 
-// Path replaces PathNode, tailored for use with a map and string identifiers.
+// Path replaces PathNode, tailored for use with a map and string identifiers
 type Path struct {
-    Identifier   string          // Identifier for the path.
-    Timestamp    time.Time       // Timestamp of the last update.
+    Identifier   string          // Identifier for the path
+    Timeout      time.Time       // Direct expiration time for the path
     Amount       uint32
-    Incoming     PeerAccount     // Details of the incoming peer.
-    Outgoing     PeerAccount     // Details of the outgoing peer.
+    Incoming     PeerAccount     // Details of the incoming peer
+    Outgoing     PeerAccount     // Details of the outgoing peer
     Commit       bool
-    CounterIn    uint32          // Counter for incoming paths.
-    CounterOut   map[string]int  // Map for outgoing counters by username.
+    CounterIn    uint32          // Counter for incoming paths
+    CounterOut   map[string]int  // Map for outgoing counters by username
 }
 
-// Account holds all path-related information and payment details.
+// Account holds all path-related information and payment details
 type Account struct {
     Username      string
     LastModified  time.Time
@@ -31,7 +31,7 @@ type Account struct {
     Payment       *Payment
 }
 
-// Payment structure adapted for use with Account.
+// Payment structure adapted for use with Account
 type Payment struct {
     Identifier  string
     Counterpart PeerAccount
