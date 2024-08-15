c121f6f8bac8fae43e8c534fd67250523ec31f55
resilience-me <emailsareobsolete@gmail.com>
2024-08-16 01:35:48 +0200

    Update write.go

--- go3/database/db_trustlines/write.go
+++ go3/database/db_trustlines/write.go
@@ -1,41 +1,42 @@
 package db_trustlines
 
 import (
+	"ripple/main"
 	"ripple/database"
 )
 
 // SetTrustlineOut sets the outbound trustline amount.
-func SetTrustlineOut(dg *Datagram, value uint32) error {
+func SetTrustlineOut(dg *main.Datagram, value uint32) error {
 	trustlineDir := database.GetTrustlineDir(dg)
 	return database.WriteUint32ToFile(trustlineDir, "trustline_out.txt", value)
 }
 
 // SetTrustlineOut sets the inbound trustline amount.
-func SetTrustlineIn(dg *Datagram, value uint32) error {
+func SetTrustlineIn(dg *main.Datagram, value uint32) error {
 	trustlineDir := database.GetTrustlineDir(dg)
 	return database.WriteUint32ToFile(trustlineDir, "trustline_in.txt", value)
 }
 
 // SetSyncCounter sets the sync_counter value.
-func SetSyncCounter(dg *Datagram, value uint32) error {
+func SetSyncCounter(dg *main.Datagram, value uint32) error {
 	trustlineDir := database.GetTrustlineDir(dg)
 	return database.WriteUint32ToFile(trustlineDir, "sync_counter.txt", value)
 }
 
 // SetSyncIn sets the sync_in value.
-func SetSyncIn(dg *Datagram, value uint32) error {
+func SetSyncIn(dg *main.Datagram, value uint32) error {
 	trustlineDir := database.GetTrustlineDir(dg)
 	return database.WriteUint32ToFile(trustlineDir, "sync_in.txt", value)
 }
 
 // SetSyncOut sets the sync_out value.
-func SetSyncOut(dg *Datagram, value uint32) error {
+func SetSyncOut(dg *main.Datagram, value uint32) error {
 	trustlineDir := database.GetTrustlineDir(dg)
 	return database.WriteUint32ToFile(trustlineDir, "sync_out.txt", value)
 }
 
 // SetTimestamp sets the sync timestamp.
-func SetTimestamp(dg *Datagram, timestamp int64) error {
+func SetTimestamp(dg *main.Datagram, timestamp int64) error {
 	trustlineDir := database.GetTrustlineDir(dg)
 	return database.WriteTimeToFile(trustlineDir, "timestamp.txt", timestamp)
 }
