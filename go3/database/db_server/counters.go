d875eb7f36bbc004b25645ccc682f2c7be791229
resilience-me <emailsareobsolete@gmail.com>
2024-08-15 02:42:00 +0200

    Update counters.go

--- go3/database/db_server/counters.go
+++ go3/database/db_server/counters.go
@@ -5,25 +5,43 @@ import (
 )
 
 // GetCounterOut retrieves the counter_out value using the datagram to determine the directory.
-func GetCounterOut(dg *Datagram) (uint32, error) {
+func GetCounterOut(dg *main.Datagram) (uint32, error) {
 	peerDir := database.GetPeerDir(dg)
 	return database.GetUint32FromFile(peerDir, "counter_out.txt")
 }
 
 // GetCounterIn retrieves the counter_in value using the datagram to determine the directory.
-func GetCounterIn(dg *Datagram) (uint32, error) {
+func GetCounterIn(dg *main.Datagram) (uint32, error) {
 	peerDir := database.GetPeerDir(dg)
 	return database.GetUint32FromFile(peerDir, "counter_in.txt")
 }
 
 // SetCounterOut sets the counter_out value.
-func SetCounterOut(dg *Datagram, value uint32) error {
+func SetCounterOut(dg *main.Datagram, value uint32) error {
 	peerDir := database.GetPeerDir(dg)
 	return database.WriteUint32ToFile(peerDir, "counter_out.txt", value)
 }
 
 // SetCounterIn sets the counter_in value.
-func SetCounterIn(dg *Datagram, value uint32) error {
+func SetCounterIn(dg *main.Datagram, value uint32) error {
 	peerDir := database.GetPeerDir(dg)
 	return database.WriteUint32ToFile(peerDir, "counter_in.txt", value)
+}
+
+// GetAndIncrementCounterOut retrieves the current counter_out, increments it, and updates the database.
+// It returns the counter value before it was incremented.
+func GetAndIncrementCounterOut(datagram *main.Datagram) (uint32, error) {
+    // Retrieve the current value of counter_out from the database.
+    counterOut, err := GetCounterOut(datagram)
+    if err != nil {
+        return 0, err  // Return error if unable to fetch the counter.
+    }
+
+    // Increment the counter and update it in the database within the same function call.
+    if err := SetCounterOut(datagram, counterOut + 1); err != nil {
+        return 0, err  // Return error if unable to update the counter.
+    }
+
+    // Return the original counter value that was fetched.
+    return counterOut, nil
 }
