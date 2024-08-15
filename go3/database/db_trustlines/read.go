8023e08905186afa7cec7e95d38b068dc24baca9
resilience-me <emailsareobsolete@gmail.com>
2024-08-16 01:35:17 +0200

    Update read.go

--- go3/database/db_trustlines/read.go
+++ go3/database/db_trustlines/read.go
@@ -1,33 +1,36 @@
 package db_trustlines
 
-import "ripple/database"
+import (
+	"ripple/main"
+	"ripple/database"
+)
 
 // GetTrustlineOut retrieves the outbound trustline using the datagram to determine the directory.
-func GetTrustlineOut(dg *Datagram) (uint32, error) {
+func GetTrustlineOut(dg *main.Datagram) (uint32, error) {
 	trustlineDir := database.GetTrustlineDir(dg)
 	return database.GetUint32FromFile(trustlineDir, "trustline_out.txt")
 }
 
 // GetTrustlineIn retrieves the inbound trustline using the datagram to determine the directory.
-func GetTrustlineIn(dg *Datagram) (uint32, error) {
+func GetTrustlineIn(dg *main.Datagram) (uint32, error) {
 	trustlineDir := database.GetTrustlineDir(dg)
 	return database.GetUint32FromFile(trustlineDir, "trustline_in.txt")
 }
 
 // GetSyncCounter retrieves the sync_counter_in value using the datagram to determine the directory.
-func GetSyncCounter(dg *Datagram) (uint32, error) {
+func GetSyncCounter(dg *main.Datagram) (uint32, error) {
 	trustlineDir := database.GetTrustlineDir(dg)
 	return database.GetUint32FromFile(trustlineDir, "sync_counter.txt")
 }
 
 // GetSyncIn retrieves the sync_in value using the datagram to determine the directory.
-func GetSyncIn(dg *Datagram) (uint32, error) {
+func GetSyncIn(dg *main.Datagram) (uint32, error) {
 	trustlineDir := database.GetTrustlineDir(dg)
 	return database.GetUint32FromFile(trustlineDir, "sync_in.txt")
 }
 
 // GetSyncOut retrieves the sync_out value using the datagram to determine the directory.
-func GetSyncOut(dg *Datagram) (uint32, error) {
+func GetSyncOut(dg *main.Datagram) (uint32, error) {
 	trustlineDir := database.GetTrustlineDir(dg)
 	return database.GetUint32FromFile(trustlineDir, "sync_out.txt")
 }
