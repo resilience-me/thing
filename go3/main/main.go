3f72663f71ad3f2990f891c4d13d03a3d340ad80
resilience-me <emailsareobsolete@gmail.com>
2024-08-16 03:23:11 +0200

    Update main.go

--- go3/main/main.go
+++ go3/main/main.go
@@ -14,8 +14,9 @@ import (
 
 // Session struct represents a network session with an optional connection
 type Session struct {
-    Datagram *Datagram // Using the same name for the identifier and the type
-    Conn     net.Conn  // Connection, can be nil if no network connection is associated
+    Datagram    *Datagram              // Using the same name for the identifier and the type
+    Conn        net.Conn               // Connection, can be nil if no network connection is associated
+    PathManager *pathfinding.PathManager // Reference to the PathManager
 }
 
 // SessionManager manages the processing of sessions
