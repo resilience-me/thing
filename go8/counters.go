e7f57325135b5ca784484c5721b2c9084e601102
resilience-me <emailsareobsolete@gmail.com>
2024-08-17 22:00:17 +0200

    Update counters.go

--- go8/counters.go
+++ go8/counters.go
@@ -6,43 +6,37 @@ import (
 
 // ValidateAndIncrementClientCounter checks if the datagram's counter is valid by comparing it to the last known counter for client connections.
 // If valid, it sets the counter to the value in the datagram to prevent replay attacks.
-func ValidateAndIncrementClientCounter(datagram *Datagram) (bool, error) {
+func ValidateAndIncrementClientCounter(datagram *Datagram) error {
 	prevCounter, err := GetCounter(datagram)
 	if err != nil {
-		return false, fmt.Errorf("error retrieving counter: %v", err)
+		return fmt.Errorf("error retrieving counter: %v", err)
 	}
-	if datagram.Counter < prevCounter {
-		return false, fmt.Errorf("replay detected or old datagram: Counter %d is not greater than the last seen counter %d", datagram.Counter, prevCounter)
-	}
-	if datagram.Counter == prevCounter {
-		return true, nil
+	if datagram.Counter <= prevCounter {
+		return fmt.Errorf("replay detected or old datagram: Counter %d is not greater than the last seen counter %d", datagram.Counter, prevCounter)
 	}
 	if err := SetCounter(datagram); err != nil {
-		return false, fmt.Errorf("failed to set counter: %v", err)
+		return fmt.Errorf("failed to set counter: %v", err)
 	}
-	return false, nil
+	return nil
 }
 
 // ValidateAndIncrementServerCounter checks if the datagram's counter is valid by comparing it to the last known counter for server connections.
 // If valid, it sets the counter to the value in the datagram to prevent replay attacks.
-func ValidateAndIncrementServerCounter(datagram *Datagram) (bool, error) {
+func ValidateAndIncrementServerCounter(datagram *Datagram) error {
 	prevCounter, err := GetCounterIn(datagram)
 	if err != nil {
-		return false, fmt.Errorf("error retrieving in-counter: %v", err)
+		return fmt.Errorf("error retrieving in-counter: %v", err)
 	}
-	if datagram.Counter < prevCounter {
-		return false, fmt.Errorf("replay detected or old datagram: Counter %d is not greater than the last seen in-counter %d", datagram.Counter, prevCounter)
-	}
-	if datagram.Counter == prevCounter {
-		return true, nil
+	if datagram.Counter <= prevCounter {
+		return fmt.Errorf("replay detected or old datagram: Counter %d is not greater than the last seen in-counter %d", datagram.Counter, prevCounter)
 	}
 	if err := SetCounterIn(datagram); err != nil {
-		return false, fmt.Errorf("failed to set in-counter: %v", err)
+		return fmt.Errorf("failed to set in-counter: %v", err)
 	}
-	return false, nil
+	return nil
 }
 
-func ValidateAndIncrementCounter(datagram *Datagram) (bool, error) {
+func ValidateAndIncrementCounter(datagram *Datagram) error {
 	if datagram.Command&0x80 == 0 { // Server session if MSB is 0
 		return ValidateAndIncrementServerCounter(datagram)
 	} 
