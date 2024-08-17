33a7081ed8e5a50f7ca5dfc062f0304f59008d97
resilience-me <emailsareobsolete@gmail.com>
2024-08-17 08:30:57 +0200

    Update counters.go

--- go7/counters.go
+++ go7/counters.go
@@ -4,9 +4,9 @@ import (
 	"fmt"
 )
 
-// validateClientCounter checks if the datagram's counter is valid by comparing it to the last known counter for client connections.
+// ValidateAndIncrementClientCounter checks if the datagram's counter is valid by comparing it to the last known counter for client connections.
 // If valid, it sets the counter to the value in the datagram to prevent replay attacks.
-func validateClientCounter(datagram *Datagram) error {
+func ValidateAndIncrementClientCounter(datagram *Datagram) error {
 	prevCounter, err := GetCounter(datagram)
 	if err != nil {
 		return fmt.Errorf("error retrieving counter: %v", err)
@@ -23,9 +23,9 @@ func validateClientCounter(datagram *Datagram) error {
 	return nil
 }
 
-// validateServerCounter checks if the datagram's counter is valid by comparing it to the last known counter for server connections.
+// ValidateAndIncrementServerCounter checks if the datagram's counter is valid by comparing it to the last known counter for server connections.
 // If valid, it sets the counter to the value in the datagram to prevent replay attacks.
-func validateServerCounter(datagram *Datagram) error {
+func ValidateAndIncrementServerCounter(datagram *Datagram) error {
 	prevCounter, err := GetCounterIn(datagram)
 	if err != nil {
 		return fmt.Errorf("error retrieving in-counter: %v", err)
@@ -40,12 +40,4 @@ func validateServerCounter(datagram *Datagram) error {
 		return fmt.Errorf("failed to set in-counter: %v", err)
 	}
 	return nil
-}
-
-// Validate the counter based on its type (client or server)
-func ValidateCounter(datagram *Datagram) error {
-	if datagram.Command&0x80 == 0 { // Server session if MSB is 0
-		return validateServerCounter(dg)
-	}
-	return validateClientCounter(dg) // Client session if MSB is 1
 }
