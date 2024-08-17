199b53c4a5718aaff8e6b7932b428c399feb03b5
resilience-me <emailsareobsolete@gmail.com>
2024-08-17 03:29:26 +0200

    Update main.go

--- go7/main.go
+++ go7/main.go
@@ -11,18 +11,17 @@ func main() {
 	sessionManager := NewSessionManager()
 
 	// Set up the UDP server
-	port := 2012
 	addr := net.UDPAddr{
-		Port: port,
+		Port: Port,
 		IP:   net.ParseIP("0.0.0.0"),
 	}
 	conn, err := net.ListenUDP("udp", &addr)
 	if err != nil {
-		fmt.Printf("Failed to listen on port %d: %v\n", port, err)
+		fmt.Printf("Failed to listen on port %d: %v\n", Port, err)
 		return
 	}
 	defer conn.Close()
 
 	// Start the server loop
-	runServerLoop(conn, transport, sessionManager, port)
+	runServerLoop(conn, transport, sessionManager)
 }
