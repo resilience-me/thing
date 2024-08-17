eee39af00116200a5c7f687aaf906a75ab3e79db
resilience-me <emailsareobsolete@gmail.com>
2024-08-17 02:52:17 +0200

    Update validator.go

--- go7/validator.go
+++ go7/validator.go
@@ -59,27 +59,25 @@ func validateServerDatagram(buf []byte, dg *Datagram) error {
 
 // ValidateDatagram parses and validates the received datagram based on its type (client or server)
 // It also validates the counter to prevent replay attacks.
-func ValidateDatagram(buffer []byte) (*Datagram, error) {
-	// Parse the datagram
-	datagram := parseDatagram(buffer)
+func ValidateDatagram(buf []byte, dg *Datagram) error {
 
 	// Validate the datagram based on its type (client or server)
-	if datagram.Command&0x80 == 0 { // Server session if MSB is 0
-		if err := validateServerDatagram(buffer, datagram); err != nil {
-			return nil, fmt.Errorf("error validating server datagram: %v", err)
+	if dg.Command&0x80 == 0 { // Server session if MSB is 0
+		if err := validateServerDatagram(buf, dg); err != nil {
+			return fmt.Errorf("error validating server datagram: %v", err)
 		}
 	} else { // Client session if MSB is 1
-		errorMessage, err := validateClientDatagram(buffer, datagram)
+		errorMessage, err := validateClientDatagram(buf, dg)
 		if err != nil {
-			return nil, fmt.Errorf("error during client datagram validation: %v", err)
+			return fmt.Errorf("error during client datagram validation: %v", err)
 		}
 	}
 
 	// Validate the counter to prevent replay attacks
-	if err := ValidateCounter(datagram); err != nil {
-		return nil, fmt.Errorf("invalid counter detected: %v", err)
+	if err := ValidateCounter(dg); err != nil {
+		return fmt.Errorf("invalid counter detected: %v", err)
 	}
 
 	// Return the validated datagram
-	return datagram, nil
+	return nil
 }
