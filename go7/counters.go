ff75115e5e9aa84b1287d17aeb2df3b85ab03417
resilience-me <emailsareobsolete@gmail.com>
2024-08-17 02:36:46 +0200

    Update counters.go

--- go7/counters.go
+++ go7/counters.go
@@ -5,7 +5,7 @@ import (
 )
 
 // ValidateCounter checks if the datagram's counter is valid by comparing it to the last known counter.
-// Returns an error if the counter is invalid (replay attack or stale datagram).
+// If valid, it sets the counter to the value in the datagram to prevent replay attacks.
 func ValidateCounter(datagram *Datagram) error {
 	var lastCounter uint32
 	var counterError error
@@ -28,5 +28,19 @@ func ValidateCounter(datagram *Datagram) error {
 		return fmt.Errorf("replay detected or old datagram: Counter %d is not greater than the last seen counter %d", datagram.Counter, lastCounter)
 	}
 
-	return nil // Counter is valid
+	// Set the counter to the value in the datagram to prevent replay attacks
+	if datagram.Command == 0x00 { // ACK datagram
+	} else if datagram.Command&0x80 == 0 { // Server connection
+		err := SetCounterIn(datagram)
+		if err != nil {
+			return fmt.Errorf("failed to set in counter: %v", err)
+		}
+	} else { // Client connection
+		err := SetCounter(datagram)
+		if err != nil {
+			return fmt.Errorf("failed to set counter: %v", err)
+		}
+	}
+
+	return nil // Counter is valid and set
 }
