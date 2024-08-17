367bbba5c8709a94dbd5a1658401cf44e08f88d4
resilience-me <emailsareobsolete@gmail.com>
2024-08-17 22:01:37 +0200

    Update validation.go

--- go8/validation.go
+++ go8/validation.go
@@ -5,41 +5,42 @@ import (
 )
 
 // checkUserAndPeerExist checks for the existence of user and peer directories
-func checkUserAndPeerExist(dg *Datagram) error {
+// It returns an error message string (empty if successful) and an error object for detailed information if an error occurs.
+func checkUserAndPeerExist(dg *Datagram) (string, error) {
     exists, err := database.CheckAccountExists(dg)
     if err != nil {
-        return fmt.Errorf("error checking account existence for user '%s': %v", dg.Username, err)
+        return "Error checking account existence", fmt.Errorf("error checking account existence for user '%s': %v", dg.Username, err)
     } else if !exists {
-        return fmt.Errorf("account directory does not exist for user '%s'", dg.Username)
+        return "User account does not exist", fmt.Errorf("account directory does not exist for user '%s'", dg.Username)
     }
 
     exists, err = database.CheckPeerExists(dg)
     if err != nil {
-        return fmt.Errorf("error checking peer existence for server '%s' and user '%s': %v", dg.PeerServerAddress, dg.PeerUsername, err)
+        return "Error checking peer existence", fmt.Errorf("error checking peer existence for server '%s' and user '%s': %v", dg.PeerServerAddress, dg.PeerUsername, err)
     } else if !exists {
-        return fmt.Errorf("peer directory does not exist for server '%s' and user '%s'", dg.PeerServerAddress, dg.PeerUsername)
+        return "Peer account does not exist", fmt.Errorf("peer directory does not exist for server '%s' and user '%s'", dg.PeerServerAddress, dg.PeerUsername)
     }
 
-    return nil // No error, directories exist
+    return "", nil // No error, directories exist
 }
 
 // validateClientDatagram validates the client datagram and checks the counter
-func validateClientDatagram(buf []byte, dg *Datagram) error {
+func validateClientDatagram(buf []byte, dg *Datagram) (string, error) {
     errorMessage, err := checkUserAndPeerExist(dg)
     if err != nil {
-        return fmt.Errorf("user and peer existence check failed: %w", err)
+        return errorMessage, fmt.Errorf("user and peer existence check failed: %w", err)
     }
 
     secretKey, err := loadClientSecretKey(dg)
     if err != nil {
-        return fmt.Errorf("loading client secret key failed: %w", err)
+        return "Error loading client secret key", fmt.Errorf("loading client secret key failed: %w", err)
     }
 
     if !verifyHMAC(buf, secretKey) {
-        return errors.New("HMAC verification failed")
+        return "Error verifying HMAC", errors.New("HMAC verification failed")
     }
 
-    return nil
+    return "", nil
 }
 
 // validateServerDatagram validates the server datagram and checks the counter
@@ -54,12 +55,4 @@ func validateServerDatagram(buf []byte, dg *Datagram) error {
     }
 
     return nil
-}
-
-func ValidateDatagram(buf []byte, dg *Datagram) error {
-	if dg.Command&0x80 == 0 { // Server session if MSB is 0
-		return validateServerDatagram(buf, dg)
-	} else {  // Client session if MSB is 1
-		return validateClientDatagram(buf, dg)
-	}
 }
