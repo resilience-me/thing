c0ea1c585aef839b40e315e4cbdaf6a938e4d411
resilience-me <emailsareobsolete@gmail.com>
2024-08-17 03:08:14 +0200

    Update client_responses.go

--- go7/client_responses.go
+++ go7/client_responses.go
@@ -5,22 +5,22 @@ import (
     "net"
 )
 
-// SendSuccessResponse sends a success message to the specified UDP address.
-func SendSuccessResponse(data []byte, conn *net.UDPConn, addr *net.UDPAddr) error {
+// SendSuccessResponse sends a success message using the provided Conn.
+func SendSuccessResponse(data []byte, conn *Conn) error {
     response := append([]byte{0}, data...) // Combine success indicator and message
 
-    if _, err := conn.WriteToUDP(response, addr); err != nil { // Send combined response to the UDP address
+    if _, err := conn.conn.WriteToUDP(response, conn.addr); err != nil { // Send combined response using the stored UDP connection and address
         return fmt.Errorf("error sending success response: %w", err) // Return detailed error
     }
 
     return nil
 }
 
-// SendErrorResponse sends an error message to the specified UDP address.
-func SendErrorResponse(message string, conn *net.UDPConn, addr *net.UDPAddr) error {
+// SendErrorResponse sends an error message using the provided Conn.
+func SendErrorResponse(message string, conn *Conn) error {
     response := append([]byte{1}, []byte(message)...) // Combine error indicator (e.g., '1') and message
 
-    if _, err := conn.WriteToUDP(response, addr); err != nil { // Send combined response to the UDP address
+    if _, err := conn.conn.WriteToUDP(response, conn.addr); err != nil { // Send combined response using the stored UDP connection and address
         return fmt.Errorf("error sending error response: %w", err) // Return detailed error
     }
 
