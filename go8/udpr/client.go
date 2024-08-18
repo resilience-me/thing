8ac6646f2e768765b5375e2d124b686abf604a2a
resilience-me <emailsareobsolete@gmail.com>
2024-08-18 11:00:00 +0200

    Update client.go

--- go8/udpr/client.go
+++ go8/udpr/client.go
@@ -45,11 +45,11 @@ func SendWithRetryClient(c *Client, data []byte, maxRetries int) error {
 	idBytes := newAck()
 	registerAck(c.ackManager, idBytes)
 
-	return sendWithRetry(c.UDPConn, c.addr, packet, idBytes, maxRetries, func(delay time.Duration) bool {
+	return sendWithRetry(c.UDPConn, c.Addr, packet, idBytes, maxRetries, func(delay time.Duration) bool {
 		// Wait for an ACK or timeout within the checkAck function
 		time.Sleep(delay)
 
 		// Check if the ACK has been received
-		return pollAck(c.ackManager, idBytes)
+		return pollAck(c.AckManager, idBytes)
 	})
 }
