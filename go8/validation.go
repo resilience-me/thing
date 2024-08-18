e2c93668c19f3df41b041fe408f337c831943cf1
resilience-me <emailsareobsolete@gmail.com>
2024-08-18 02:25:07 +0200

    Update validation.go

--- go8/validation.go
+++ go8/validation.go
@@ -4,16 +4,9 @@ import (
 	"fmt"
 )
 
-// checkUserAndPeerExist checks for the existence of user and peer directories
+// checkPeerExist checks for the existence of user and peer directories
 // It returns an error message string (empty if successful) and an error object for detailed information if an error occurs.
-func checkUserAndPeerExist(dg *Datagram) (string, error) {
-    exists, err := database.CheckAccountExists(dg)
-    if err != nil {
-        return "Error checking account existence", fmt.Errorf("error checking account existence for user '%s': %v", dg.Username, err)
-    } else if !exists {
-        return "User account does not exist", fmt.Errorf("account directory does not exist for user '%s'", dg.Username)
-    }
-
+func checkPeerExist(dg *Datagram) (string, error) {
     exists, err = database.CheckPeerExists(dg)
     if err != nil {
         return "Error checking peer existence", fmt.Errorf("error checking peer existence for server '%s' and user '%s': %v", dg.PeerServerAddress, dg.PeerUsername, err)
