048e570361c5082eaa090efcd7482835a1cf04ba
resilience-me <emailsareobsolete@gmail.com>
2024-08-21 13:54:18 +0200

    Update client_responses.go

--- go10/comm/client_responses.go
+++ go10/comm/client_responses.go
@@ -9,7 +9,7 @@ import (
 func SendSuccessResponse(data []byte, conn *Conn) error {
 	response := append([]byte{0}, data...) // Combine success indicator and message
 
-	if err := udpr.SendWithRetry(conn.UDPConn, conn.addr, response, udpr.HighImportance); err != nil {
+	if err := udpr.SendWithRetry(conn.UDPConn, conn.addr, response, HighImportance); err != nil {
 		return fmt.Errorf("error sending success response: %w", err) // Return detailed error
 	}
 
@@ -20,7 +20,7 @@ func SendSuccessResponse(data []byte, conn *Conn) error {
 func SendErrorResponse(message string, conn *Conn) error {
 	response := append([]byte{1}, []byte(message)...) // Combine error indicator and message
 
-	if err := udpr.SendWithRetry(conn.UDPConn, conn.addr, response, udpr.HighImportance); err != nil {
+	if err := udpr.SendWithRetry(conn.UDPConn, conn.addr, response, HighImportance); err != nil {
 		return fmt.Errorf("error sending error response: %w", err) // Return detailed error
 	}
 
