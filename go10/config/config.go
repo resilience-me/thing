938e2c9449a4cd00ad157a8ec88cca70db72fcb4
resilience-me <emailsareobsolete@gmail.com>
2024-08-21 14:41:03 +0200

    Update config.go

--- go10/config/config.go
+++ go10/config/config.go
@@ -1,5 +1,23 @@
 package config
 
+import (
+    "os"
+    "path/filepath"
+)
+
 const (
     Port = 2012
 )
+
+var datadir = filepath.Join(os.Getenv("HOME"), "ripple")
+var serverAddress string
+
+// GetServerAddress returns the server address as a string
+func GetServerAddress() string {
+    return serverAddress
+}
+
+// GetDataDir returns the datadir as a string
+func GetDataDir() string {
+    return datadir
+}
