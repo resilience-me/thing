8137e4f4c31d5b8d3be1f93ca3a951ad9d493fdc
resilience-me <emailsareobsolete@gmail.com>
2024-08-21 13:44:28 +0200

    Update protocol.go

--- go10/main/protocol.go
+++ go10/main/protocol.go
@@ -1,16 +1,5 @@
 package main
 
-// Datagram holds the structure of the incoming data
-type Datagram struct {
-    Command           byte
-    Username          string
-    PeerUsername      string
-    PeerServerAddress string
-    Arguments         [256]byte
-    Counter           uint32
-    Signature         [32]byte
-}
-
 // CommandHandler defines the type for command handling functions
 type CommandHandler func(session Session)
 
