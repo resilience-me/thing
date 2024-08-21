080cde3c883b023ec1f911bade7176e18df77578
resilience-me <emailsareobsolete@gmail.com>
2024-08-21 13:20:16 +0200

    Create getTrustlineIn.go

---
+++ go10/handlers/trustlines/client_trustlines/getTrustlineIn.go
@@ -0,0 +1,32 @@
+package client_trustlines
+
+import (
+    "log"
+    "ripple/database/db_trustlines"
+    "ripple/main"
+    "ripple/comm"
+)
+
+// GetTrustlineIn handles fetching the inbound trustline information
+func GetTrustlineIn(session main.Session) {
+    datagram := session.Datagram
+
+    // Fetch the inbound trustline
+    trustline, err := db_trustlines.GetTrustlineIn(datagram)
+    if err != nil {
+        log.Printf("Error reading inbound trustline for user %s: %v", datagram.Username, err)
+        comm.SendErrorResponse("Error reading inbound trustline.", session.Conn)
+        return
+    }
+
+    // Prepare success response
+    responseData := main.Uint32ToBytes(trustline)
+
+    // Send the success response back to the client
+    if err := comm.SendSuccessResponse(responseData, session.Conn); err != nil {
+        log.Printf("Error sending success response to user %s: %v", datagram.Username, err)
+        return
+    }
+
+    log.Printf("Inbound trustline sent successfully to user %s.", datagram.Username)
+}
