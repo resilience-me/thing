f420028e06c6c239dcbeebf5b908260ca2ad6851
resilience-me <emailsareobsolete@gmail.com>
2024-08-21 17:25:12 +0200

    Update client_responses.go

--- go10/comm/client_responses.go
+++ go10/comm/client_responses.go
@@ -3,7 +3,7 @@ package comm
 import "fmt"
 
 // SendSuccessResponse sends a success message using the provided address with retry logic.
-func SendSuccessResponse(data []byte, addr *net.UDPAddr) error {
+func SendSuccessResponse(addr *net.UDPAddr, data []byte) error {
 	response := append([]byte{0}, data...) // Combine success indicator and message
 	if err := SendWithAddress(addr, response, HighImportance); err != nil {
 		return fmt.Errorf("error sending success response: %w", err)
@@ -12,7 +12,7 @@ func SendSuccessResponse(data []byte, addr *net.UDPAddr) error {
 }
 
 // SendErrorResponse sends an error message using the provided address with retry logic.
-func SendErrorResponse(message string, addr *net.UDPAddr) error {
+func SendErrorResponse(addr *net.UDPAddr, message string) error {
 	response := append([]byte{1}, []byte(message)...) // Combine error indicator and message
 	if err := SendWithAddress(addr, response, HighImportance); err != nil {
 		return fmt.Errorf("error sending error response: %w", err)
