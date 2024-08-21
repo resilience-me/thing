699a38552358a170eab5a063fc1268427aab0ed8
resilience-me <emailsareobsolete@gmail.com>
2024-08-21 20:00:15 +0200

    Create commands.go

---
+++ go10/commands/commands.go
@@ -0,0 +1,9 @@
+package commands
+
+const (
+    ClientTrustlines_SetTrustline      = 0
+    ClientTrustlines_SyncTrustlineIn   = 1
+    ClientTrustlines_SyncTrustlineOut  = 2
+    ClientTrustlines_GetTrustlineIn    = 3
+    ClientTrustlines_GetTrustlineOut   = 4
+)
