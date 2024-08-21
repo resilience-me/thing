82858b1b7ed6827020fca26b7656522c07a7890e
resilience-me <emailsareobsolete@gmail.com>
2024-08-21 17:18:41 +0200

    Update udpr_wrappers.go

--- go10/comm/udpr_wrappers.go
+++ go10/comm/udpr_wrappers.go
@@ -11,14 +11,9 @@ const (
 	HighImportance   = 12 // 12 retries for priority messages
 )
 
-// SendWithResolvedAddressAndConn resolves the address, creates a new UDP connection, and sends data with retries.
-func SendWithResolvedAddressAndConn(address string, data []byte, maxRetries int) error {
-	// Resolve the destination address to a UDP address
-	addr, err := net.ResolveUDPAddr("udp", fmt.Sprintf("%s:%d", address, config.Port))
-	if err != nil {
-		return fmt.Errorf("failed to resolve address '%s:%d': %w", address, config.Port, err)
-	}
-
+// SendWithAddress sends data to a specified UDP address with retry logic.
+// It handles the creation and closure of the UDP connection internally.
+func SendWithAddress(addr *net.UDPAddr, data []byte, maxRetries int) error {
 	// Create a UDP connection with an ephemeral local port
 	conn, err := net.DialUDP("udp", nil, addr)
 	if err != nil {
@@ -26,18 +21,33 @@ func SendWithResolvedAddressAndConn(address string, data []byte, maxRetries int)
 	}
 	defer conn.Close()
 
-	// Call the SendWithRetry function with the resolved address and the newly created connection
-	return udpr.SendWithRetry(conn, addr, data, maxRetries)
+	// Use the udpr.SendWithRetry to send the data
+	if err := udpr.SendWithRetry(conn, addr, data, maxRetries); err != nil {
+		return fmt.Errorf("error sending data: %w", err)
+	}
+
+	return nil
+}
+
+// SendWithResolvedAddressAndConn resolves the address, creates a new UDP connection, and sends data with retries.
+func SendWithResolvedAddress(address string, data []byte, maxRetries int) error {
+	// Resolve the destination address to a UDP address
+	addr, err := net.ResolveUDPAddr("udp", fmt.Sprintf("%s:%d", address, config.Port))
+	if err != nil {
+		return fmt.Errorf("failed to resolve address '%s:%d': %w", address, config.Port, err)
+	}
+	// Call SendWithAddress function with the resolved address
+	return SendWithAddress(addr, data, maxRetries)
 }
 
 // Default Send with standard importance (5 retries)
 func Send(destinationAddr string, data []byte) error {
-	return SendWithResolvedAddressAndConn(destinationAddr, data, LowImportance)
+	return SendWithResolvedAddress(destinationAddr, data, LowImportance)
 }
 
 // Send with priority importance (12 retries)
 func SendPriority(destinationAddr string, data []byte) error {
-	return SendWithResolvedAddressAndConn(destinationAddr, data, HighImportance)
+	return SendWithResolvedAddress(destinationAddr, data, HighImportance)
 }
 
 // Wrapper for udpr.SendAck
