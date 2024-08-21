71f6cf6a60658c2a2ba0292b5d299ce2b07f6c55
resilience-me <emailsareobsolete@gmail.com>
2024-08-21 16:59:55 +0200

    Create shutdown.go

---
+++ go10/main/shutdown.go
@@ -0,0 +1,36 @@
+package main
+
+import (
+    "fmt"
+    "log"
+    "os"
+    "os/signal"
+    "syscall"
+    "sync/atomic"
+)
+
+// Ensure that the shutdown process is also communicated clearly
+func shutdownHandler(shutdownFlag *int32) {
+    interruptCount := 0 // Scoped to this function
+    signals := make(chan os.Signal, 1)
+    signal.Notify(signals, syscall.SIGINT, syscall.SIGTERM)
+
+    for sig := range signals {
+        interruptCount++
+        log.Printf("Signal received: %v", sig)  // Logging the signal for better traceability
+
+        if interruptCount == 1 {
+            fmt.Println("Interrupt received, initiating graceful shutdown...")
+            fmt.Println("Press Ctrl+C up to 9 times in total to force quit immediately.")
+            atomic.StoreInt32(shutdownFlag, 1)  // Signal to shutdown the manager and other components
+            continue           // Skip to the next iteration
+        }
+
+        if interruptCount == 9 {
+            fmt.Println("Force quitting after 9 interrupts...")
+            os.Exit(1) // Force exit after receiving 9 interrupts
+        }
+
+        fmt.Printf("Interrupt received (%d/9), press Ctrl+C again to force quit...\n", interruptCount)
+    }
+}
