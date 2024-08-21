c221dfaf49f932bc9f7dd1197baaf09f082d5b1d
resilience-me <emailsareobsolete@gmail.com>
2024-08-21 17:53:01 +0200

    Update session.go

--- go10/main/session.go
+++ go10/main/session.go
@@ -80,7 +80,7 @@ func (sm *SessionManager) handleSession(session *Session) {
 	// But only if the command included a peer. The range 0x00 to 0x3F is allocated for commands that do.
 	if command&0xC0 == 0 { // Bit 7 (MSB) is 0, bit 6 is 0
 	    if errorMessage, err := auth.ValidatePeerExists(datagram); err != nil {
-	        log.Printf("Error vaslidating peer existence for user %s: %v", username, err)
+	        log.Printf("Error validating peer existence for user %s: %v", username, err)
 	        comm.SendErrorResponse(errorMessage, session.Conn)
 	        return
 	    }
