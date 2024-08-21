7b2c3f56b437ea2c62dbaaeeed7e40e28a5be0fe
resilience-me <emailsareobsolete@gmail.com>
2024-08-21 13:58:38 +0200

    Create getTrustlineOut.go

---
+++ go10/handlers/trustlines/client_trustlines/getTrustlineOut.go
@@ -0,0 +1,33 @@
+package client_trustlines
+
+import (
+    "log"
+    "ripple/database/db_trustlines"
+    "ripple/main"
+    "ripple/comm"
+    "ripple/types"
+)
+
+// GetTrustlineOut handles fetching the outbound trustline information
+func GetTrustlineOut(session main.Session) {
+    datagram := session.Datagram
+
+    // Fetch the outbound trustline
+    trustline, err := db_trustlines.GetTrustlineOut(datagram)
+    if err != nil {
+        log.Printf("Error reading outbound trustline for user %s: %v", datagram.Username, err)
+        comm.SendErrorResponse("Error reading outbound trustline.", session.Conn)
+        return
+    }
+
+    // Prepare success response using the main utility function
+    responseData := types.Uint32ToBytes(trustline)
+
+    // Send the success response back to the client
+    if err := comm.SendSuccessResponse(responseData, session.Conn); err != nil {
+        log.Printf("Error sending success response to user %s: %v", datagram.Username, err)
+        return
+    }
+
+    log.Printf("Outbound trustline sent successfully to user %s.", datagram.Username) // Log successful operation
+}
