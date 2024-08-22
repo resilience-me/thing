f32cebe51bdf52994e906c7ff88a44f22574d592
resilience-me <emailsareobsolete@gmail.com>
2024-08-22 10:30:24 +0200

    Update counters.go

--- go10/database/counters.go
+++ go10/database/counters.go
@@ -39,21 +39,3 @@ func SetCounterOut(username, peerServerAddress, peerUsername string, value uint3
 	peerDir := GetPeerDir(username, peerServerAddress, peerUsername)
 	return WriteUint32ToFile(peerDir, "counter_out.txt", value)
 }
-
-// GetAndIncrementCounterOut retrieves the current counter_out, increments it, and updates the database.
-// It returns the counter value before it was incremented.
-func GetAndIncrementCounterOut(username, peerServerAddress, peerUsername string) (uint32, error) {
-    // Retrieve the current value of counter_out from the database.
-    counterOut, err := GetCounterOut(username, peerServerAddress, peerUsername)
-    if err != nil {
-        return 0, err  // Return error if unable to fetch the counter.
-    }
-
-    // Increment the counter and update it in the database within the same function call.
-    if err := SetCounterOut(username, peerServerAddress, peerUsername, counterOut + 1); err != nil {
-        return 0, err  // Return error if unable to update the counter.
-    }
-
-    // Return the original counter value that was fetched.
-    return counterOut, nil
-}
