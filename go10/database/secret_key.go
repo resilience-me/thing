819ed3009e345d3e7bd8089f04074c77708d86ab
resilience-me <emailsareobsolete@gmail.com>
2024-08-22 15:17:18 +0200

    Update secret_key.go

--- go10/database/secret_key.go
+++ go10/database/secret_key.go
@@ -11,11 +11,13 @@ func loadSecretKeyFromDir(dir string) ([]byte, error) {
     return secretKey, nil
 }
 
+// LoadSecretKey loads the secret key for the given username.
 func LoadSecretKey(username string) ([]byte, error) {
     accountDir := GetAccountDir(username)
     return loadSecretKeyFromDir(accountDir)
 }
 
+// LoadPeerSecretKey loads the peer's secret key.
 func LoadPeerSecretKey(username, peerServerAddress, peerUsername string) ([]byte, error) {
     peerDir := GetPeerDir(dg.Username, dg.PeerServerAddress, dg.PeerUsername)
     return loadSecretKeyFromDir(peerDir)
