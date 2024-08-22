100cc1e245b41045d496c609f80c8e66d0f60a85
resilience-me <emailsareobsolete@gmail.com>
2024-08-22 09:30:08 +0200

    Update wrappers.go

--- go10/database/wrappers.go
+++ go10/database/wrappers.go
@@ -7,12 +7,17 @@ func GetAccountDirFromDatagram(dg *types.Datagram) string {
     return GetAccountDir(dg.Username)
 }
 
-// GetPeerDirFromDatagram constructs the peer directory path from the datagram and returns it
-func GetPeerDirFromDatagram(dg *types.Datagram) string {
+// GetPeerDirFromIncomingDatagram constructs the peer directory path from the incoming datagram and returns it
+func GetPeerDirFromIncomingDatagram(dg *types.Datagram) string {
     return GetPeerDir(dg.Username, dg.PeerServerAddress, dg.PeerUsername)
 }
 
-// GetPeerDirFromOutgoingDatagram constructs the peer directory path from the datagram and returns it
+// GetPeerDirFromOutgoingDatagram constructs the peer directory path from the outgoing datagram and returns it
 func GetPeerDirFromOutgoingDatagram(dg *types.Datagram, peerServerAddress string) string {
     return GetPeerDir(dg.PeerUsername, peerServerAddress, dg.Username)
+}
+
+// Wrapper for GetPeerDirFromIncomingDatagram
+func GetPeerDirFromDatagram(dg *types.Datagram) string {
+    return GetPeerDirFromIncomingDatagram(dg)
 }
