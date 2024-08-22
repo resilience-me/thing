25b4adf45c15f369a406dd8aa457ba6bcc444939
resilience-me <emailsareobsolete@gmail.com>
2024-08-22 10:14:12 +0200

    Update counters.go

--- go10/auth/counters.go
+++ go10/auth/counters.go
@@ -3,19 +3,20 @@ package auth
 import (
 	"fmt"
 	"ripple/types"
+	"ripple/database"
 )
 
 // ValidateAndIncrementClientCounter checks if the datagram's counter is valid by comparing it to the last known counter for client connections.
 // If valid, it sets the counter to the value in the datagram to prevent replay attacks.
 func ValidateAndIncrementClientCounter(datagram *types.Datagram) error {
-	prevCounter, err := GetCounter(datagram)
+	prevCounter, err := database.GetCounter(datagram)
 	if err != nil {
 		return fmt.Errorf("error retrieving counter: %v", err)
 	}
 	if datagram.Counter <= prevCounter {
 		return fmt.Errorf("replay detected or old datagram: Counter %d is not greater than the last seen counter %d", datagram.Counter, prevCounter)
 	}
-	if err := SetCounter(datagram); err != nil {
+	if err := database.SetCounter(datagram); err != nil {
 		return fmt.Errorf("failed to set counter: %v", err)
 	}
 	return nil
@@ -24,14 +25,14 @@ func ValidateAndIncrementClientCounter(datagram *types.Datagram) error {
 // ValidateAndIncrementServerCounter checks if the datagram's counter is valid by comparing it to the last known counter for server connections.
 // If valid, it sets the counter to the value in the datagram to prevent replay attacks.
 func ValidateAndIncrementServerCounter(datagram *types.Datagram) error {
-	prevCounter, err := GetCounterIn(datagram)
+	prevCounter, err := database.GetCounterIn(datagram)
 	if err != nil {
 		return fmt.Errorf("error retrieving in-counter: %v", err)
 	}
 	if datagram.Counter <= prevCounter {
 		return fmt.Errorf("replay detected or old datagram: Counter %d is not greater than the last seen in-counter %d", datagram.Counter, prevCounter)
 	}
-	if err := SetCounterIn(datagram); err != nil {
+	if err := database.SetCounterIn(datagram); err != nil {
 		return fmt.Errorf("failed to set in-counter: %v", err)
 	}
 	return nil
