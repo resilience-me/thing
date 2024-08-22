4111333c1367c6d76746ad7cbcca14fe81c6357a
resilience-me <emailsareobsolete@gmail.com>
2024-08-22 11:38:33 +0200

    Create counters.go

---
+++ go10/handlers/trustlines/counters.go
@@ -0,0 +1,42 @@
+package trustlines
+
+import (
+    "fmt"
+    "ripple/types"
+    "ripple/database/db_trustlines"
+)
+
+// IncrementSyncCounter retrieves the current sync_counter, increments it, and updates the database.
+// It returns an error if something goes wrong during the process.
+func IncrementSyncCounter(datagram *types.Datagram) error {
+    // Retrieve the current value of sync_counter from the database.
+    syncCounter, err := db_trustlines.GetSyncCounter(datagram)
+    if err != nil {
+        return err  // Return error if unable to fetch the sync_counter.
+    }
+
+    // Increment the counter and update it in the database within the same function call.
+    if err := db_trustlines.SetSyncCounter(datagram, syncCounter + 1); err != nil {
+        return err  // Return error if unable to update the sync_counter.
+    }
+
+    // No need to return any value; just indicate success by returning nil.
+    return nil
+}
+
+// GetSyncStatus retrieves the syncCounter and syncOut values and returns the syncCounter and whether they are equal.
+func GetSyncStatus(datagram *types.Datagram) (uint32, bool, error) {
+    // Retrieve the current syncCounter value
+    syncCounter, err := db_trustlines.GetSyncCounter(datagram)
+    if err != nil {
+        return 0, false, fmt.Errorf("Error getting syncCounter for user %s: %v", datagram.Username, err)
+    }
+
+    // Retrieve the current syncOut value
+    syncOut, err := db_trustlines.GetSyncOut(datagram)
+    if err != nil {
+        return 0, false, fmt.Errorf("Error getting syncOut for user %s: %v", datagram.Username, err)
+    }
+
+    return syncCounter, syncOut == syncCounter, nil
+}
