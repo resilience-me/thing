d1386e6fab83800e02e408d06c1e2043f8d8b306
resilience-me <emailsareobsolete@gmail.com>
2024-08-22 09:27:53 +0200

    Update database.go

--- go10/database/database.go
+++ go10/database/database.go
@@ -11,25 +11,10 @@ func GetAccountDir(username string) string {
     return filepath.Join(datadir, "accounts", username)
 }
 
-// GetAccountDirFromDatagram constructs the account directory path from the datagram
-func GetAccountDirFromDatagram(dg *Datagram) string {
-    return GetAccountDir(dg.Username)
-}
-
 // GetPeerDir constructs the peer directory path from a username, peer server address and peer username
 func GetPeerDir(username, peerServerAddress, peerUsername string) string {
     accountDir := GetAccountDir(username)
     return filepath.Join(accountDir, "peers", peerServerAddress, peerUsername)
-}
-
-// GetPeerDirFromDatagram constructs the peer directory path from the datagram and returns it
-func GetPeerDirFromDatagram(dg *Datagram) string {
-    return GetPeerDir(dg.Username, dg.PeerServerAddress, dg.PeerUsername)
-}
-
-// GetPeerDirFromOutgoingDatagram constructs the peer directory path from the datagram and returns it
-func GetPeerDirFromOutgoingDatagram(dg *Datagram, peerServerAddress string) string {
-    return GetPeerDir(dg.PeerUsername, peerServerAddress, dg.Username)
 }
 
 // GetTrustlineDir constructs the trustline directory path from the datagram and returns it.
