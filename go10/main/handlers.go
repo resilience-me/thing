61f975a6a181e3d136ee5a90a22e62f7234547d6
resilience-me <emailsareobsolete@gmail.com>
2024-08-22 12:49:36 +0200

    Update handlers.go

--- go10/main/handlers.go
+++ go10/main/handlers.go
@@ -5,5 +5,10 @@ type CommandHandler func(session Session)
 
 // CommandHandlers maps command bytes to handler functions
 var commandHandlers = [256]CommandHandler{
+    0:   client_trustlines.SetTrustline,     // Client Command
+    1:   client_trustlines.SyncTrustlineIn,  // Client Command
+    2:   client_trustlines.SyncTrustlineOut, // Client Command
+    3:   client_trustlines.GetTrustlineIn,   // Client Command
+    4:   client_trustlines.GetTrustlineOut,  // Client Command
     // Other indices are nil by default
 }
