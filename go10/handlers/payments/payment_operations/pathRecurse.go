f71fde791652ecf6a13cd52ea60cb9960e40d5a4
resilience-me <emailsareobsolete@gmail.com>
2024-08-24 01:57:14 +0200

    Update pathRecurse.go

--- go10/handlers/payments/payment_operations/pathRecurse.go
+++ go10/handlers/payments/payment_operations/pathRecurse.go
@@ -4,28 +4,20 @@ import (
     "log"
     "ripple/types"
     "ripple/handlers"
-    "ripple/comm"
     "ripple/commands"
     "ripple/pathfinding"
 )
 
-// FindPathRecurse sends a PathFindingRecurse command to the specified peer using the depth and identifier from the datagram.
+// PathRecurse sends a PathFindingRecurse command to the specified peer using the depth and identifier from the datagram.
 func PathRecurse(datagram *types.Datagram, peer pathfinding.PeerAccount, depth uint32) {
     // Create the arguments slice by appending the depth to the identifier from the datagram
     arguments := append(datagram.Arguments[:32], types.Uint32ToBytes(depth)...)
 
-    // Prepare the datagram for forwarding with the command and updated arguments
-    newDatagram, err := handlers.PrepareDatagram(commands.ServerPayments_PathRecurse, datagram.Username, peer.ServerAddress, peer.Username, arguments)
-    if err != nil {
-        log.Printf("Failed to prepare datagram: %v", err)
+    // Prepare, sign, and send the datagram using the helper function from the handlers package
+    if err := handlers.PrepareAndSendDatagram(commands.ServerPayments_PathRecurse, datagram.Username, peer.ServerAddress, peer.Username, arguments); err != nil {
+        log.Printf("Failed to prepare and send PathRecurse command from %s to peer %s at server %s: %v", datagram.Username, peer.Username, peer.ServerAddress, err)
         return
     }
 
-    // Sign and send the datagram to the target peer
-    if err := comm.SignAndSendDatagram(newDatagram, peer.ServerAddress); err != nil {
-        log.Printf("Failed to sign and send FindPathRecurse command to %s at %s: %v", peer.Username, peer.ServerAddress, err)
-        return
-    }
-
-    log.Printf("Successfully signed and sent FindPathRecurse command to %s at %s", peer.Username, peer.ServerAddress)
+    log.Printf("Successfully signed and sent PathRecurse command from %s to peer %s at server %s", datagram.Username, peer.Username, peer.ServerAddress)
 }
