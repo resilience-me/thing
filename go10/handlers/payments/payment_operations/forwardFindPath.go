5c295f4f6fd23139dfb349431b3e3eb91da56b19
resilience-me <emailsareobsolete@gmail.com>
2024-08-24 00:20:09 +0200

    Update and rename findPath.go to forwardFindPath.go

--- go10/handlers/payments/payment_operations/forwardFindPath.go
+++ go10/handlers/payments/payment_operations/forwardFindPath.go
@@ -11,8 +11,8 @@ import (
     "ripple/database/db_pathfinding"
 )
 
-// FindPath forwards the pathfinding request to all connected peers
-func FindPath(datagram *types.Datagram, inOrOut byte) {
+// ForwardFindPath forwards the pathfinding request to all connected peers
+func ForwardFindPath(datagram *types.Datagram, inOrOut byte) {
     // Retrieve the list of connected peers
     peers, err := db_pathfinding.GetPeers(datagram.Username)
     if err != nil {
