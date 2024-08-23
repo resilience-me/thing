8d6962fdc138c33acb429b2f86f72466c8bc271d
resilience-me <emailsareobsolete@gmail.com>
2024-08-23 22:31:27 +0200

    Update wrappers.go

--- go10/database/db_trustlines/wrappers.go
+++ go10/database/db_trustlines/wrappers.go
@@ -21,3 +21,12 @@ func SetTrustlineOutFromDatagram(dg *types.Datagram, value uint32) (uint32, erro
 func SetTrustlineInFromDatagram(dg *types.Datagram, value uint32) (uint32, error) {
 	return SetTrustlineIn(dg.Username, dg.PeerServerAddress, dg.PeerUsername, value)
 }
+
+// GetTrustline retrieves the trustline (either incoming or outgoing) based on the inOrOut parameter.
+func GetTrustline(username, peerServerAddress, peerUsername string, inOrOut byte) (uint32, error) {
+    if inOrOut == 0 { // Assume 0 means incoming trustline
+        return GetTrustlineIn(username, peerServerAddress, peerUsername)
+    } else { // Assume 1 means outgoing trustline
+        return GetTrustlineOut(username, peerServerAddress, peerUsername)
+    }
+}
