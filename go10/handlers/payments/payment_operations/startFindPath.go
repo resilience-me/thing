0b96c1e86ec260b3214ae53b46e770b49639f62b
resilience-me <emailsareobsolete@gmail.com>
2024-08-24 00:27:03 +0200

    Create startFindPath.go

---
+++ go10/handlers/payments/payment_operations/startFindPath.go
@@ -0,0 +1,38 @@
+func StartFindPath(username, identifier string, amount uint32, command, inOrOut byte) {
+    // Retrieve the list of connected peers
+    peers, err := db_pathfinding.GetPeers(username)
+    if err != nil {
+        log.Printf("Failed to retrieve peers for user %s: %v", username, err)
+        return
+    }
+
+    arguments := append([]byte(someIdentifier), types.Uint32ToBytes(amount)...)
+
+    for _, peer := range peers {
+        // Check if the trustline is sufficient
+        sufficient, err := payments.CheckTrustlineSufficient(username, peer.ServerAddress, peer.Username, amount, inOrOut)
+        if err != nil {
+            log.Printf("Error checking trustline: %v", err)
+            continue
+        }
+        if !sufficient {
+            log.Printf("Trustline insufficient for user %s with peer %s at %s", username, peer.Username, peer.ServerAddress)
+            continue
+        }
+
+        // Create a new datagram for each peer
+        newDatagram, err := handlers.PrepareDatagram(command, username, peer.ServerAddress, peer.Username, arguments)
+        if err != nil {
+            log.Printf("Failed to prepare datagram: %v", err)
+            continue
+        }
+
+        // Serialize and sign the datagram
+        if err := comm.SignAndSendDatagram(newDatagram, peer.ServerAddress); err != nil {
+            log.Printf("Failed to send pathfinding request to %s at %s: %v", peer.Username, peer.ServerAddress, err)
+            return // Exit early on error
+        }
+
+        log.Printf("Sent pathfinding request to %s at %s", peer.Username, peer.ServerAddress)
+    }
+}
