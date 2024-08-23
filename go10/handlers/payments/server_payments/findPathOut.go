3cb0f297e3f30c15d8fb6e0686d7347daad62e25
resilience-me <emailsareobsolete@gmail.com>
2024-08-23 22:49:51 +0200

    Update findPathOut.go

--- go10/handlers/payments/server_payments/findPathOut.go
+++ go10/handlers/payments/server_payments/findPathOut.go
@@ -49,40 +49,11 @@ func FindPathOut(session *Session) {
         log.Printf("Initialized new path for identifier: %s with amount: %d", pathIdentifier, pathAmount)
 
         // Since this is the first time seeing this path, send a PathFindingRecurse back to the buyer
-        findPathOutRecurse(datagram, path)
+        findPathOutRecurse(datagram, path.Incoming)
         return
     }
 
     // If the path is already present, forward the PathFinding request to peers
     log.Printf("Path already exists for identifier %s, forwarding to peers", pathIdentifier)
     payments.ForwardFindPath(datagram, 1)
-}
-
-// findPathOutRecurse sends a PathFindingRecurse command back to the buyer
-func findPathOutRecurse(datagram *types.Datagram, path *pathfinding.Path) {
-    // Directly target the incoming peer, which represents the direction back to the buyer
-    targetPeer := path.Incoming
-
-    // Prepare the datagram for forwarding
-    newDatagram, err := handlers.PrepareDatagram(datagram.Username, targetPeer.ServerAddress, targetPeer.Username)
-    if err != nil {
-        log.Printf("Failed to prepare datagram: %v", err)
-        return
-    }
-
-    // Set the command directly to ServerPayments_FindPathRecurse
-    newDatagram.Command = commands.ServerPayments_FindPathRecurse
-
-    // Copy the path identifier from path.Identifier to the new datagram's Arguments field
-    copy(newDatagram.Arguments[:32], []byte(path.Identifier)) // Assuming path.Identifier is at most 32 bytes
-
-    // No need to update the depth in newDatagram.Arguments[32:36] since it is already zero
-
-    // Sign and send the datagram to the target peer
-    if err := comm.SignAndSendDatagram(newDatagram, targetPeer.ServerAddress); err != nil {
-        log.Printf("Failed to sign and send FindPathRecurse command to %s at %s: %v", targetPeer.Username, targetPeer.ServerAddress, err)
-        return
-    }
-
-    log.Printf("Successfully signed and sent FindPathRecurse command to %s at %s", targetPeer.Username, targetPeer.ServerAddress)
 }
