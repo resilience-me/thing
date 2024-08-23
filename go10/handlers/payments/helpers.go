eec114363ff6e6474342a6b5d4d77436e7b5f0e0
resilience-me <emailsareobsolete@gmail.com>
2024-08-23 21:03:03 +0200

    Update helpers.go

--- go10/handlers/payments/helpers.go
+++ go10/handlers/payments/helpers.go
@@ -30,3 +30,29 @@ func CheckTrustlineInSufficient(username, peerServerAddress, peerUsername string
 
     return true, nil
 }
+
+// CheckTrustlineOutSufficient checks if the outgoing trustline is sufficient for the given amount.
+// It returns true if the trustline is sufficient, false otherwise, along with any error encountered.
+func CheckTrustlineOutSufficient(username, peerServerAddress, peerUsername string, amount uint32) (bool, error) {
+    // Retrieve the incoming trustline
+    trustlineOut, err := db_trustlines.GetTrustlineOut(username, peerServerAddress, peerUsername)
+    if err != nil {
+        return false, fmt.Errorf("failed to retrieve incoming trustline for user %s with peer %s at %s: %v", username, peerUsername, peerServerAddress, err)
+    }
+
+    // Retrieve the outgoing credit line
+    creditLineIn, err := db_trustlines.GetCreditLineIn(username, peerServerAddress, peerUsername)
+    if err != nil {
+        return false, fmt.Errorf("failed to retrieve outgoing credit line for user %s with peer %s at %s: %v", username, peerUsername, peerServerAddress, err)
+    }
+
+    // Adjust the trustline by subtracting the outgoing credit line
+    availableAmount := trustlineOut - creditLineIn
+
+    // Check if the adjusted trustline is sufficient
+    if availableAmount < amount {
+        return false, nil
+    }
+
+    return true, nil
+}
