b3f8e84e63ae07996d9bec8d09ddd04fd642ece8
resilience-me <emailsareobsolete@gmail.com>
2024-08-23 22:39:29 +0200

    Update forwardFindPath.go

--- go10/handlers/payments/forwardFindPath.go
+++ go10/handlers/payments/forwardFindPath.go
@@ -12,42 +12,48 @@ import (
 )
 
 // ForwardFindPath forwards the pathfinding request to all connected peers
-func ForwardFindPath(datagram *types.Datagram) {
+func forwardFindPath(datagram *types.Datagram, inOrOut byte) {
+
+    username := datagram.Username
+
     // Retrieve the list of connected peers
-    peers, err := db_pathfinding.GetPeers(datagram.Username)
+    peers, err := db_pathfinding.GetPeers(username)
     if err != nil {
-        log.Printf("Failed to retrieve peers for user %s: %v", datagram.Username, err)
+        log.Printf("Failed to retrieve peers for user %s: %v", username, err)
         return
     }
 
-    // Extract command and arguments outside of the loop for efficiency
+    // Extract datagram fields outside of the loop for efficiency
     command := datagram.Command
     arguments := datagram.Arguments[:]
     
+    peerUsername := datagram.PeerUsername
+    peerServerAddress := datagram.PeerServerAddress
+
     // Extract the path amount from the datagram arguments
     amount := binary.BigEndian.Uint32(arguments[32:36])
 
     for _, peer := range peers {
         // Skip if this peer is the one from which the datagram was received
-        if peer.Username == datagram.PeerUsername && peer.ServerAddress == datagram.PeerServerAddress {
+        if peer.Username == peerUsername && peer.ServerAddress == peerServerAddress {
             continue
         }
 
-        // Check if the trustline (in or out) is sufficient
-        sufficient, err := CheckTrustlineSufficient(datagram.Username, peer.ServerAddress, peer.Username, amount, command)
+        // Check if the trustline is sufficient
+        sufficient, err := payments.CheckTrustlineSufficient(username, peer.ServerAddress, peer.Username, amount, inOrOut)
         if err != nil {
             log.Printf("Error checking trustline: %v", err)
             continue
         }
         if !sufficient {
-            log.Printf("Trustline insufficient for user %s with peer %s at %s", datagram.Username, peer.Username, peer.ServerAddress)
+            log.Printf("Trustline insufficient for user %s with peer %s at %s", username, peer.Username, peer.ServerAddress)
             continue
         }
 
-        // Use PrepareDatagramFull to create the new datagram
-        newDatagram, err := handlers.PrepareDatagram(command, datagram.Username, peer.ServerAddress, peer.Username, arguments)
+        // Use PrepareDatagram to create the new datagram with command and arguments
+        newDatagram, err := handlers.PrepareDatagram(command, username, peer.ServerAddress, peer.Username, arguments)
         if err != nil {
-            log.Printf("Failed to prepare pathfinding datagram: %v", err)
+            log.Printf("Failed to prepare datagram: %v", err)
             continue
         }
 
