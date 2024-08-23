2697b03c7f603850e0fcf37e9c0cc569ff1902f2
resilience-me <emailsareobsolete@gmail.com>
2024-08-23 23:38:27 +0200

    Update forwardFindPathRecurse.go

--- go10/handlers/payments/forwardFindPathRecurse.go
+++ go10/handlers/payments/forwardFindPathRecurse.go
@@ -9,10 +9,13 @@ import (
     "ripple/pathfinding"
 )
 
-// ForwardFindPathRecurse sends a PathFindingRecurse command to the specified peer.
-func ForwardFindPathRecurse(datagram *types.Datagram, peer pathfinding.PeerAccount) {    
-    // Prepare the datagram for forwarding with the command and arguments
-    newDatagram, err := handlers.PrepareDatagram(commands.ServerPayments_FindPathRecurse, datagram.Username, peer.ServerAddress, peer.Username, datagram.Arguments[:])
+// ForwardFindPathRecurse sends a PathFindingRecurse command to the specified peer using the depth and identifier from the datagram.
+func ForwardFindPathRecurse(datagram *types.Datagram, peer pathfinding.PeerAccount, depth uint32) {
+    // Create the arguments slice by appending the depth to the identifier from the datagram
+    arguments := append(datagram.Arguments[:32], types.Uint32ToBytes(depth)...)
+
+    // Prepare the datagram for forwarding with the command and updated arguments
+    newDatagram, err := handlers.PrepareDatagram(commands.ServerPayments_FindPathRecurse, datagram.Username, peer.ServerAddress, peer.Username, arguments)
     if err != nil {
         log.Printf("Failed to prepare datagram: %v", err)
         return
