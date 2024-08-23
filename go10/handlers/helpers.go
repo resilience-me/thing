7659319b791d5ad5e3a10a7fae0839a476159772
resilience-me <emailsareobsolete@gmail.com>
2024-08-23 21:58:06 +0200

    Update helpers.go

--- go10/handlers/helpers.go
+++ go10/handlers/helpers.go
@@ -20,6 +20,19 @@ func PrepareDatagram(username, peerServerAddress, peerUsername string) (*types.D
     return dg, nil
 }
 
+// PrepareDatagramFull prepares a datagram with all necessary fields including the command and arguments.
+func PrepareDatagramFull(username, peerServerAddress, peerUsername string, command byte, arguments []byte) (*types.Datagram, error) {
+    // Prepare the new datagram
+    datagram, err := handlers.PrepareDatagram(datagram.Username, peer.ServerAddress, peer.Username)
+    if err != nil {
+        return nil, fmt.Errorf("Failed to prepare datagram: %v", err)
+    }
+    datagram.Command = command
+    copy(dg.Arguments[:], arguments)
+
+    return datagram, nil
+}
+
 // PrepareDatagramResponse calls PrepareDatagram with fields from an incoming datagram
 func PrepareDatagramResponse(dg *types.Datagram) (*types.Datagram, error) {
     return PrepareDatagram(dg.Username, dg.PeerServerAddress, dg.PeerUsername)
