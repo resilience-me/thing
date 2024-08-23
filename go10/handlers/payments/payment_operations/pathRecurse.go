40aab8fa959bfe4ec5f1c35c8815aead2787ce1c
resilience-me <emailsareobsolete@gmail.com>
2024-08-24 01:34:31 +0200

    Update and rename findPathRecurse.go to pathRecurse.go

--- go10/handlers/payments/payment_operations/pathRecurse.go
+++ go10/handlers/payments/payment_operations/pathRecurse.go
@@ -10,12 +10,12 @@ import (
 )
 
 // FindPathRecurse sends a PathFindingRecurse command to the specified peer using the depth and identifier from the datagram.
-func FindPathRecurse(datagram *types.Datagram, peer pathfinding.PeerAccount, depth uint32) {
+func PathRecurse(datagram *types.Datagram, peer pathfinding.PeerAccount, depth uint32) {
     // Create the arguments slice by appending the depth to the identifier from the datagram
     arguments := append(datagram.Arguments[:32], types.Uint32ToBytes(depth)...)
 
     // Prepare the datagram for forwarding with the command and updated arguments
-    newDatagram, err := handlers.PrepareDatagram(commands.ServerPayments_FindPathRecurse, datagram.Username, peer.ServerAddress, peer.Username, arguments)
+    newDatagram, err := handlers.PrepareDatagram(commands.ServerPayments_PathRecurse, datagram.Username, peer.ServerAddress, peer.Username, arguments)
     if err != nil {
         log.Printf("Failed to prepare datagram: %v", err)
         return
