d82da130cdd142648f87cd9e7dae3f3a87ae560e
resilience-me <emailsareobsolete@gmail.com>
2024-08-24 08:47:17 +0200

    Update startFindPath.go

--- go10/handlers/payments/payment_operations/startFindPath.go
+++ go10/handlers/payments/payment_operations/startFindPath.go
@@ -3,9 +3,10 @@ package payment_operations
 import (
     "log"
     "ripple/handlers"
-    "ripple/handlers/payments"
     "ripple/types"
     "ripple/database/db_pathfinding"
+    "ripple/handlers/payments"
+    "ripple/handlers/payments/payment_operations"
 )
 
 // StartFindPath initiates a pathfinding request to all connected peers.
@@ -22,7 +23,7 @@ func StartFindPath(username, identifier string, amount uint32, inOrOut byte) {
 
     for _, peer := range peers {
         // Use the new helper function to check the trustline and send the datagram
-        if err := handlers.CheckTrustlineAndSendFindPathDatagram(command, username, peer.ServerAddress, peer.Username, amount, inOrOut, arguments); err != nil {
+        if err := payment_operations.CheckTrustlineAndSendFindPathDatagram(command, username, peer.ServerAddress, peer.Username, amount, inOrOut, arguments); err != nil {
             log.Printf("Error processing datagram: %v", err)
             continue
         }
