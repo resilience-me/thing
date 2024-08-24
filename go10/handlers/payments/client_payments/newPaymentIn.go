537d419596e6cc91ba180dd44be90d57914181a8
resilience-me <emailsareobsolete@gmail.com>
2024-08-24 03:54:15 +0200

    Create newPaymentIn.go

---
+++ go10/handlers/payments/client_payments/newPaymentIn.go
@@ -0,0 +1,13 @@
+package client_payments
+
+import (
+    "log"
+    "ripple/main"
+    "ripple/types"
+    "ripple/handlers/payments/payment_operations"
+)
+
+// NewPaymentIn handles the command to initiate a new incoming payment.
+func NewPaymentIn(session main.Session) {
+    payment_operations.NewPayment(session, types.Incoming)
+}
