50325b6783ef71ea115ce8f3b7d33d3bf859d4bc
resilience-me <emailsareobsolete@gmail.com>
2024-08-24 04:47:25 +0200

    Update commands.go

--- go10/commands/commands.go
+++ go10/commands/commands.go
@@ -6,4 +6,15 @@ const (
     ClientTrustlines_SyncTrustlineOut  = 2
     ClientTrustlines_GetTrustlineIn    = 3
     ClientTrustlines_GetTrustlineOut   = 4
+    ClientPayments_NewPaymentOut       = 5
+    ClientPayments_NewPaymentIn        = 6
+    ClientPayments_GetPayment          = 7
+
+    ServerTrustlines_SetTrustline      = 127
+    ServerTrustlines_GetTrustline      = 128
+    ServerTrustlines_SetSyncOut        = 129
+    ServerTrustlines_SetTimestamp      = 130
+    ServerPayments_FindPathOut         = 131
+    ServerPayments_FindPathIn          = 132
+    ServerPayments_PathRecurse         = 133
 )
