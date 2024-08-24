96e69838ba02923610746d229710e3929f3be6c4
resilience-me <emailsareobsolete@gmail.com>
2024-08-24 07:39:22 +0200

    Update findPathIn.go

--- go10/handlers/payments/server_payments/findPathIn.go
+++ go10/handlers/payments/server_payments/findPathIn.go
@@ -1,12 +1,11 @@
 package server_payments
 
 import (
-    "ripple/main"
     "ripple/types"
     "ripple/handlers/payments/payment_operations"
 )
 
 // FindPathIn processes a pathfinding request from the seller to the buyer
-func FindPathIn(session main.Session) {
+func FindPathIn(session types.Session) {
     payment_operations.FindPath(session.Datagram, types.Incoming)
 }
