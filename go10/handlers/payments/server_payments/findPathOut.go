a6cf6fd147b1e35a19f69b60d0271974c0f81152
resilience-me <emailsareobsolete@gmail.com>
2024-08-24 03:55:59 +0200

    Update findPathOut.go

--- go10/handlers/payments/server_payments/findPathOut.go
+++ go10/handlers/payments/server_payments/findPathOut.go
@@ -1,8 +1,12 @@
 package server_payments
 
-import "ripple/handlers/payments/payment_operations"
+import (
+    "ripple/main"
+    "ripple/types"
+    "ripple/handlers/payments/payment_operations"
+)
 
 // FindPathOut processes a pathfinding request from the buyer to the seller
-func FindPathOut(session *Session) {
+func FindPathOut(session main.Session) {
     payment_operations.FindPath(session.Datagram, types.Outgoing)
 }
