c07480328cdb951f1e1907b5ed37d4404e0d759b
resilience-me <emailsareobsolete@gmail.com>
2024-08-24 04:45:47 +0200

    Update handlers.go

--- go10/main/handlers.go
+++ go10/main/handlers.go
@@ -12,5 +12,16 @@ var commandHandlers = [256]CommandHandler{
     2:   client_trustlines.SyncTrustlineOut, // Client Command
     3:   client_trustlines.GetTrustlineIn,   // Client Command
     4:   client_trustlines.GetTrustlineOut,  // Client Command
+    5:   client_payments.NewPaymentOut,      // Client Command
+    6:   client_payments.NewPaymentIn,       // Client Command
+    7:   client_payments.GetPayment,         // Client Command
+
+    127: server_trustlines.SetTrustline,     // Server Command
+    128: server_trustlines.GetTrustline,     // Server Command
+    129: server_trustlines.SetSyncOut,       // Server Command
+    130: server_trustlines.SetTimestamp,     // Server Command
+    131: server_payments.FindPathOut,        // Server Command
+    132: server_payments.FindPathIn    ,     // Server Command
+    133: server_payments.PathRecurse  ,      // Server Command
     // Other indices are nil by default
 }
