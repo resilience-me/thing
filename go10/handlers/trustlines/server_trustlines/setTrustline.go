0a38fa02f0f489788093893165b18e5b36142f76
resilience-me <emailsareobsolete@gmail.com>
2024-08-24 04:41:25 +0200

    Update setTrustline.go

--- go10/handlers/trustlines/server_trustlines/setTrustline.go
+++ go10/handlers/trustlines/server_trustlines/setTrustline.go
@@ -22,52 +22,37 @@ func SetTrustline(session main.Session) {
     }
 
     // Retrieve the syncIn counter from the Datagram
-    syncIn := types.BytesToUint32(datagram.Arguments[4:8])
+    syncInBytes := datagram.Arguments[4:8]
+    syncIn := types.BytesToUint32(syncInBytes)
 
     if syncIn > prevSyncIn {
-        handleTrustlineUpdate(session, syncIn)
+
+        // Retrieve the trustline amount from the Datagram
+        trustlineAmount := types.BytesToUint32(datagram.Arguments[:4])
+    
+        // Update the trustline, sync_in, and timestamp
+        if err := db_trustlines.SetTrustlineIn(datagram, trustlineAmount); err != nil {
+            log.Printf("Error writing trustline to file for user %s: %v", datagram.Username, err)
+            return
+        }
+    
+        if err := db_trustlines.SetSyncIn(datagram, syncIn); err != nil {
+            log.Printf("Error writing sync_in to file for user %s: %v", datagram.Username, err)
+            return
+        }
+    
+        log.Printf("Trustline and sync_in updated successfully for user %s.", datagram.Username)
+    
+        // Prepare and send the datagram to sync the peer's out counter
+        if err := handlers.PrepareAndSendDatagram(commands.ServerTrustlines_SetSyncOut, datagram.Username, datagram.PeerServerAddress, datagram.PeerUsername, syncInBytes); err != nil {
+            log.Printf("Failed to sign and send datagram for user %s: %v", datagram.Username, err)
+            return
+        }
+    
+        log.Printf("Trustline update and datagram sent successfully for user %s.", datagram.Username)        
     } else {
-        handleTimestampOnly(session)
+        log.Printf("Sync_in is synchronized with the peer's most recent trustline_out for user %s.", datagram.Username)
     }
-}
-
-func handleTrustlineUpdate(session main.Session, syncIn uint32) {
-    datagram := session.Datagram
-
-    // Retrieve the trustline amount from the Datagram
-    trustlineAmount := types.BytesToUint32(datagram.Arguments[:4])
-
-    // Update the trustline, sync_in, and timestamp
-    if err := db_trustlines.SetTrustlineIn(datagram, trustlineAmount); err != nil {
-        log.Printf("Error writing trustline to file for user %s: %v", datagram.Username, err)
-        return
-    }
-
-    if err := db_trustlines.SetSyncIn(datagram, syncIn); err != nil {
-        log.Printf("Error writing sync_in to file for user %s: %v", datagram.Username, err)
-        return
-    }
-
-    if err := db_trustlines.SetTimestamp(datagram, time.Now().Unix()); err != nil {
-        log.Printf("Error writing timestamp to file for user %s: %v", datagram.Username, err)
-        return
-    }
-
-    log.Printf("Trustline, sync_in, and timestamp updated successfully for user %s.", datagram.Username)
-
-    // Prepare and send the datagram to sync the peer's out counter
-    if err := handlers.PrepareAndSendDatagram(commands.ServerTrustlines_SetSyncOut, datagram.PeerUsername, datagram.PeerServerAddress, datagram.Username, types.Uint32ToBytes(syncIn)); err != nil {
-        log.Printf("Failed to sign and send datagram for user %s: %v", datagram.Username, err)
-        return
-    }
-
-    log.Printf("Trustline update and datagram sent successfully for user %s.", datagram.Username)
-}
-
-func handleTimestampOnly(session main.Session) {
-    datagram := session.Datagram
-
-    log.Printf("Sync_in is synchronized with the peer's most recent trustline_out for user %s.", datagram.Username)
 
     if err := db_trustlines.SetTimestamp(datagram, time.Now().Unix()); err != nil {
         log.Printf("Error writing timestamp to file for user %s: %v", datagram.Username, err)
