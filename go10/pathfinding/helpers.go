75bbe0f730ce14d8c92a77904ffb5d1d3e8afc4e
resilience-me <emailsareobsolete@gmail.com>
2024-08-24 03:46:47 +0200

    Create helpers.go

---
+++ go10/pathfinding/helpers.go
@@ -0,0 +1,19 @@
+import (
+    "sync"
+    "time"
+)
+
+func FetchAndRefresh() *Account {
+    pm.mu.Lock()
+    defer pm.mu.Unlock()
+
+    if account, exists := pm.Accounts[username]; exists {
+        newCleanup := time.Now().Add(config.PathFindingTimeout)
+        // Ensure reinsert does not lower Cleanup timer for an account currently committed to a payment
+        if newCleanup.After(account.Cleanup) {
+            account.Cleanup = newCleanup
+        }
+        return account
+    }
+    return nil
+}
