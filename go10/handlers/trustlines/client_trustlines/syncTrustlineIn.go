ff800a4a61b520cbbb78206fbdb5c9d7119d211c
resilience-me <emailsareobsolete@gmail.com>
2024-08-24 07:22:06 +0200

    Update syncTrustlineIn.go

--- go10/handlers/trustlines/client_trustlines/syncTrustlineIn.go
+++ go10/handlers/trustlines/client_trustlines/syncTrustlineIn.go
@@ -19,7 +19,7 @@ func SyncTrustlineIn(session types.Session) {
     dgOut, err := handlers.PrepareDatagramResponse(datagram)
     if err != nil {
         log.Printf("Error preparing datagram for user %s: %v", datagram.Username, err)
-        comm.SendErrorResponse("Error preparing datagram.", session.Addr)
+        comm.SendErrorResponse(session.Addr, "Error preparing datagram.")
         return
     }
 
@@ -27,7 +27,7 @@ func SyncTrustlineIn(session types.Session) {
     syncIn, err := db_trustlines.GetSyncIn(datagram)
     if err != nil {
         log.Printf("Error getting sync_in for user %s: %v", datagram.Username, err)
-        comm.SendErrorResponse("Failed to read sync_in value.", session.Addr)
+        comm.SendErrorResponse(session.Addr, "Failed to read sync_in value.")
         return
     }
 
@@ -38,12 +38,12 @@ func SyncTrustlineIn(session types.Session) {
     // Send the GetTrustline command to the peer server
     if err := comm.SignAndSendDatagram(dgOut, datagram.PeerServerAddress); err != nil {
         log.Printf("Failed to send GetTrustline command for user %s to peer %s: %v", datagram.Username, datagram.PeerUsername, err)
-        comm.SendErrorResponse("Failed to send GetTrustline command.", session.Addr)
+        comm.SendErrorResponse(session.Addr, "Failed to send GetTrustline command.")
         return
     }
 
     // Send success response to the client
-    if err := comm.SendSuccessResponse([]byte("Trustline sync request sent successfully."), session.Addr); err != nil {
+    if err := comm.SendSuccessResponse(session.Addr, []byte("Trustline sync request sent successfully.")); err != nil {
         log.Printf("Failed to send success response to user %s: %v", datagram.Username, err)
         return
     }
