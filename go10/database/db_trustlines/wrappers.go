eec3bc79da7ed69f61bde664a90ef0fea83d94f9
resilience-me <emailsareobsolete@gmail.com>
2024-08-24 08:40:14 +0200

    Update wrappers.go

--- go10/database/db_trustlines/wrappers.go
+++ go10/database/db_trustlines/wrappers.go
@@ -30,3 +30,12 @@ func GetTrustline(username, peerServerAddress, peerUsername string, inOrOut byte
         return GetTrustlineOut(username, peerServerAddress, peerUsername)
     }
 }
+
+// GetCreditline retrieves the creditline (either incoming or outgoing) based on the inOrOut parameter.
+func GetCreditline(username, peerServerAddress, peerUsername string, inOrOut byte) (uint32, error) {
+    // if inOrOut == 0 { // Assume 0 means incoming trustline
+    //     return GetCreditlineIn(username, peerServerAddress, peerUsername)
+    // } else { // Assume 1 means outgoing trustline
+    //     return GetCreditlineOut(username, peerServerAddress, peerUsername)
+    // }
+}
