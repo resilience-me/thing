9bc7282493ae98685325a1eb7eab59aae3074b44
resilience-me <emailsareobsolete@gmail.com>
2024-08-24 07:26:43 +0200

    Update getTrustlineIn.go

--- go10/handlers/trustlines/client_trustlines/getTrustlineIn.go
+++ go10/handlers/trustlines/client_trustlines/getTrustlineIn.go
@@ -13,7 +13,7 @@ func GetTrustlineIn(session types.Session) {
     datagram := session.Datagram
 
     // Fetch the inbound trustline
-    trustline, err := db_trustlines.GetTrustlineIn(datagram)
+    trustline, err := db_trustlines.GetTrustlineInFromDatagram(datagram)
     if err != nil {
         log.Printf("Error reading inbound trustline for user %s: %v", datagram.Username, err)
         comm.SendErrorResponse(session.Addr, "Error reading inbound trustline.")
